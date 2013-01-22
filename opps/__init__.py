0641c1fba12ac7e59d28654ed4e9bf16816101c5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-01-22 04:47:25 -0200

    Add init var opps package

---
+++ opps/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 0, 1)
+
+__version__ = ".".join(map(str, VERSION))
+__status__ = "Development"
+__description__ = u"Opps CMS websites magazines and high-traffic"
+
+__author__ = u"Thiago Avelino"
+__credits__ = []
+__email__ = u"opps-developers@googlegroups.com"
+__license__ = u"BSD"
+__copyright__ = u"Copyright 2013, Opps Project"
