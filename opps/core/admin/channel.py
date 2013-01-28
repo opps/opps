85cf22ff02a7c4af576d6553ae721d174364e357
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-01-28 00:43:16 -0200

    Add basic core admin on Channel

---
+++ opps/core/admin/channel.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from django.contrib import admin
+
+from opps.core.models import Channel
+
+class ChannelAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(Channel, ChannelAdmin)
