9feb2192fb53d325a7d604b0b51fa0de1272014e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-03 05:33:48 -0200

    Create profile models

---
+++ opps/core/models/profile.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+
+class Profile(models.Model):
+
+    user = models.ForeignKey(User, related_name='user')
+    twitter = models.CharField(_(u"Twitter"), max_length=75, blank=True,
+            null=True)
+
+    class Meta:
+        app_label = 'core'
+
+    def __unicode__(self):
+        return self.user
