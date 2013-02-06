f2a1f74f76d501c3a92e83399aade8b34db7187d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-06 03:06:27 -0200

    Add source module on Opps Core

--- opps/core/models/__init__.py
+++ opps/core/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from opps.core.models.channel import *
 from opps.core.models.profile import *
+from opps.core.models.source import *
 from opps.core.models.publisher import *
