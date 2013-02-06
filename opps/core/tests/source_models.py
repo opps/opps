d19d0468b33029c1f2b730dba5ad0a6c50177d43
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-06 03:17:23 -0200

    write test check create test site on source model core

--- opps/core/tests/source_models.py
+++ opps/core/tests/source_models.py
@@ -13,3 +13,11 @@ class SourceModelTest(TestCase):
         self.source = Source.objects.create(name=u'Test site',
                 slug=u'test-site')
 
+    def test_check_create_test_site(self):
+        """
+        Check exist source test site, create on setup class
+        """
+        source = Source.objects.filter(slug='test-site').get()
+        self.assertTrue(source)
+        self.assertEqual(source, self.source)
+
