868ee88ad98cb44839cfab56ce3a2520bc7e930d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-06 03:03:09 -0200

    Clean profile admin, source obsolete

--- opps/core/admin/profile.py
+++ opps/core/admin/profile.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.contrib.auth.models import User
 
 from opps.core.models import Profile
 
@@ -8,10 +7,4 @@ class ProfileAdmin(admin.ModelAdmin):
     pass
 
 
-def user_unicode(self):
-    return  u'{0}, {1}'.format(self.last_name, self.first_name)
-User.__unicode__ = user_unicode
-
-admin.site.unregister(User)
-admin.site.register(User)
 admin.site.register(Profile, ProfileAdmin)
