bd3d9d06dd55d38faf69ed28525dfe2e7fa0a977
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-06 03:04:43 -0200

    Create basic source models stores website base article

---
+++ opps/core/models/source.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+
+class Source(models.Model):
+
+    name = models.CharField(_(u"Name"), max_length=75)
+    slug = models.SlugField(_(u"Slug"), max_length=100, unique=True,
+            db_index=True)
+    url = models.URLField(_(u'URL'), max_length=200, blank=True, null=True)
+    feed = models.URLField(_(u'URL'), max_length=200, blank=True, null=True)
+
+
+    class Meta:
+        app_label = 'core'
+
+    def __unicode__(self):
+        return self.slug
