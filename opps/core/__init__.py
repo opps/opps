8c8bc1ef8e3ba7519d4612856a420ed410974e12
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 10:59:56 -0300

    add redactor on installed apps settings

--- opps/core/__init__.py
+++ opps/core/__init__.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+
+
 trans_app_label = _('Opps')
+settings.INSTALLED_APPS += ('redactor',)
