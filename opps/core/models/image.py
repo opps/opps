4a1a61bcfd662abbf3ef2d400accd0d85416bf61
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 17:42:55 -0300

    remove validators not used on image model core

--- opps/core/models/image.py
+++ opps/core/models/image.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.core.validators import MinValueValidator, MaxValueValidator
 
 from opps.core.models.published import Published
 from opps.core.models.date import Date
