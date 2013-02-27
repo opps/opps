cb78ef105431a0d4af5d2071a9ff4910f6c65596
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 15:57:55 -0300

    Add new field on image models on core

--- opps/core/models/image.py
+++ opps/core/models/image.py
@@ -10,6 +10,13 @@ from opps.core.models.date import Date
 
 class Image(Published, Date):
 
+    title = models.CharField(_(u"Title"), max_length=140)
+    slug = models.SlugField(_(u"Slug"), max_length=150, blank=True)
     image = models.ImageField(upload_to="opps_images/")
     description = models.CharField(_(u"Description"), max_length=255,
             null=True, blank=True)
+
+    credit = models.CharField(_(u"Credit"), max_length=255, blank=False)
+
+    def __unicode__(self):
+        return self.title
