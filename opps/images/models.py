1a9b05b2e4ee2c222b54e524de496b4d6a299aa5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 03:25:29 -0300

    Remove image crop app

--- opps/images/models.py
+++ opps/images/models.py
@@ -6,37 +6,10 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from opps.core.models.published import Published
 from opps.core.models.date import Date
 
-from sorl.thumbnail import ImageField, get_thumbnail
-
-
-
-CROP_TYPE = (("center", u"center"),)
-
-class ImageCrop(models.Model):
-
-    type = models.CharField(_(u"Type"), max_length=15, choices=CROP_TYPE)
-    width = models.IntegerField()
-    height = models.IntegerField()
-    quality = models.IntegerField(validators=[MinValueValidator(20),
-        MaxValueValidator(100)])
-
-    def __unicode__(self):
-        return "{0} x {1} ({2})".format(self.width, self.height, self.type)
 
 
 class Image(Published, Date):
 
-    image = ImageField(upload_to="opps_images/")
+    image = models.ImageField(upload_to="opps_images/")
     description = models.CharField(_(u"Description"), max_length=255,
             null=True, blank=True)
-    crop = models.ManyToManyField(ImageCrop, null=True, blank=True)
-
-    def save(self, *args, **kwargs):
-        if self.crop:
-            import pdb; pdb.set_trace()
-            for x in self.crop.all():
-                get_thumbnail(self.image,
-                        "{0}x{1}".format(x.width, x.height),
-                        crop=x.type, quality=x.quality)
-
-        super(Image, self).save(*args, **kwargs)
