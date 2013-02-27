09347e329796415ecf9855e8967fb4e3136cb9f8
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 14:17:13 -0300

    Auto set user (auth) on save post mdoels (django admin)

--- opps/core/admin/article.py
+++ opps/core/admin/article.py
@@ -7,4 +7,9 @@ from opps.core.models import Post
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user:
+            obj.user = request.user
+        obj.save()
 admin.site.register(Post, PostAdmin)
