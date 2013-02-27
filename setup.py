131cead153dd29cacf03fbf841f26fc85482b57c
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 10:28:00 -0300

    Set version redactor 0.2 on setup file

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = ["Django==1.5",
                     "thumbor==3.7.1",
                     "django-tagging==0.3.1",
                     "django-redis",
-                    "django-redactor"]
+                    "django-redactor==0.2"]
 
 classifiers = ["Development Status :: 4 - Beta",
     "Intended Audience :: Developers",
