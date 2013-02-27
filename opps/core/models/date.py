78a55cfce436ce24b840515a1582cd0efe7623fd
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 00:38:58 -0300

    Create date models, control insert and update

---
+++ opps/core/models/date.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from datetime import datetime
+
+
+
+class Date(models.Model):
+
+    date_insert = models.DateTimeField(_(u"Date insert"), auto_now_add=True)
+    date_update = models.DateTimeField(_(u"Date update"), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.date_update = datetime.now()
+        super(Publisher, self).save(*args, **kwargs)
