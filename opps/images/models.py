94e9817115159c887c1b8bca8f5637133a8b05f4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 02:58:37 -0300

    Add core models date abstract on Image app

--- opps/images/models.py
+++ opps/images/models.py
@@ -4,6 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from opps.core.models.published import Published
+from opps.core.models.date import Date
 
 from sorl.thumbnail import ImageField, get_thumbnail
 
@@ -23,7 +24,7 @@ class ImageCrop(models.Model):
         return "{0} x {1} ({2})".format(self.width, self.height, self.type)
 
 
-class Image(Publisher):
+class Image(Published, Date):
 
     image = ImageField(upload_to="opps_images/")
     description = models.CharField(_(u"Description"), max_length=255,
