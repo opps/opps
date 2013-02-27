a4d5a74c675b0c1d90dd9f254367975af9ea2735
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 01:01:02 -0300

    Change package core mdoels init

--- opps/core/models/__init__.py
+++ opps/core/models/__init__.py
@@ -2,4 +2,4 @@
 from opps.core.models.channel import *
 from opps.core.models.profile import *
 from opps.core.models.source import *
-from opps.core.models.publisher import *
+from opps.core.models.published import *
