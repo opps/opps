d30c3b9c574566d9c69fc1322b6a2dfec3a6eb67
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-27 21:10:19 -0300

    Create post admin form, custom content field add texteditor

--- opps/core/admin/article.py
+++ opps/core/admin/article.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django import forms
 
 from opps.core.models import Post
 
+from redactor.widgets import RedactorEditor
+
+
+
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        widgets = {'content': RedactorEditor(),}
 
 
 class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
     prepopulated_fields = {"slug": ("title",)}
 
     def save_model(self, request, obj, form, change):
