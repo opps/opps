d0cea10352baf2f33df65e058d4410878012994c
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-02-28 19:00:46 -0300

    use post image on admin inline

--- opps/core/admin/article.py
+++ opps/core/admin/article.py
@@ -2,10 +2,20 @@
 from django.contrib import admin
 from django import forms
 
-from opps.core.models import Post
+from opps.core.models import Post, PostImage
+from opps.core.models import Image
 
 from redactor.widgets import RedactorEditor
 
+
+
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    fk_name = 'post'
+    raw_id_fields = ['image']
+    actions = None
+    extra = 1
+    fieldsets = [(None, {'fields': ('image', 'order')})]
 
 
 class PostAdminForm(forms.ModelForm):
@@ -17,6 +27,7 @@ class PostAdminForm(forms.ModelForm):
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
     prepopulated_fields = {"slug": ("title",)}
+    inlines = [PostImageInline]
 
     def save_model(self, request, obj, form, change):
         if not obj.user:
