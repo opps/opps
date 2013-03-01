88586c5f41bf81ee4695931cd766c78afaf50037
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-01 06:11:42 -0300

    fix pep8 on setup

--- setup.py
+++ setup.py
@@ -16,15 +16,15 @@ install_requires = ["Django==1.5",
                     "django-wysiwyg-redactor==0.3.0"]
 
 classifiers = ["Development Status :: 4 - Beta",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: BSD License",
-    "Operating System :: OS Independent",
-    "Framework :: Django",
-    'Programming Language :: Python',
-    "Programming Language :: Python :: 2.7",
-    "Operating System :: OS Independent",
-    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    'Topic :: Software Development :: Libraries :: Python Modules']
+               "Intended Audience :: Developers",
+               "License :: OSI Approved :: BSD License",
+               "Operating System :: OS Independent",
+               "Framework :: Django",
+               'Programming Language :: Python',
+               "Programming Language :: Python :: 2.7",
+               "Operating System :: OS Independent",
+               "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+               'Topic :: Software Development :: Libraries :: Python Modules']
 
 try:
     long_description = open('README.md').read()
@@ -32,18 +32,17 @@ except:
     long_description = opps.__description__
 
 setup(name='opps',
-    version = opps.__version__,
-    description = opps.__description__,
-    long_description = long_description,
-    classifiers = classifiers,
-    keywords = 'opps cms django apps magazines websites',
-    author = opps.__author__,
-    author_email = opps.__email__,
-    url = 'http://oppsproject.org',
-    download_url = "https://github.com/avelino/opps/tarball/master",
-    license = opps.__license__,
-    packages = find_packages(exclude=('doc', 'docs',)),
-    package_dir = {'opps': 'opps'},
-    install_requires = install_requires,
-    include_package_data = True,
-)
+        version=opps.__version__,
+        description=opps.__description__,
+        long_description=long_description,
+        classifiers=classifiers,
+        keywords='opps cms django apps magazines websites',
+        author=opps.__author__,
+        author_email=opps.__email__,
+        url='http://oppsproject.org',
+        download_url="https://github.com/avelino/opps/tarball/master",
+        license=opps.__license__,
+        packages=find_packages(exclude=('doc', 'docs',)),
+        package_dir={'opps': 'opps'},
+        install_requires=install_requires,
+        include_package_data=True,)
