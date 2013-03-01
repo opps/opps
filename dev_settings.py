f1b40958d9a782f28e5c950963c2102843127d87
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-01 05:55:05 -0300

    E123 closing bracket does not match indentation of opening bracket's line

--- dev_settings.py
+++ dev_settings.py
@@ -34,7 +34,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.request",
-    )
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
