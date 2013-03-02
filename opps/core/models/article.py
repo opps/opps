edbd9f11027bc38e9e681f646e9622b466cdda4d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-02 00:38:48 -0300

    create post source models is through

--- opps/core/models/article.py
+++ opps/core/models/article.py
@@ -2,9 +2,10 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from opps.core.models.publishable import Publishable
-from opps.core.models.channel import Channel
+from opps.core.models import Publishable
+from opps.core.models import Channel
 from opps.core.models.image import Image
+from opps.core.models import Source
 
 from tagging.fields import TagField
 
@@ -26,6 +27,9 @@ class Article(Publishable):
             blank=False, null=True, on_delete=models.SET_NULL,
             related_name='article_main_image')
 
+    sources = models.ManyToManyField(Source, null=True, blank=True,
+            related_name='post_sources', through='PostSource')
+
     tags = TagField(null=True, verbose_name=_(u"Tags"))
 
 
@@ -40,7 +44,6 @@ class Post(Article):
 
     images = models.ManyToManyField(Image, null=True, blank=True,
             related_name='post_images', through='PostImage')
-    credit = models.CharField(_("Credit"), blank=True, max_length=255)
 
     class Meta:
         app_label = 'core'
@@ -58,3 +61,18 @@ class PostImage(models.Model):
 
     class Meta:
         app_label = 'core'
+
+
+class PostSource(models.Model):
+    post = models.ForeignKey(Post, verbose_name=_(u'Post'), null=True,
+            related_name='postsource_post', on_delete=models.SET_NULL)
+    source = models.ForeignKey(Source, verbose_name=_(u'Source'), null=True,
+            related_name='postsource_source', on_delete=models.SET_NULL)
+    order = models.PositiveIntegerField(_(u'Order'), default=0)
+
+    def __unicode__(self):
+        return self.source.slug
+
+    class Meta:
+        app_label = 'core'
+
