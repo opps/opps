7acff1700d04a2b07dab9703a047713bd8f27c25
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-04 16:48:27 -0300

    Auto set user on upload image

--- opps/core/admin/image.py
+++ opps/core/admin/image.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.models import User
 
 from opps.core.models.image import Image
 
 
 class ImagesAdmin(admin.ModelAdmin):
-    pass
+    exclude = ('user',)
+
+    def save_model(self, request, obj, form, change):
+        try:
+            if obj.user:
+                pass
+        except User.DoesNotExist:
+            obj.user = request.user
+
+        super(ImagesAdmin, self).save_model(request, obj, form, change)
+
 
 
 admin.site.register(Image, ImagesAdmin)
