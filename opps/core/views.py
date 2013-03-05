d8880d682d29e63f0c9499c8367f7b8cefc715cd
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 02:03:53 -0300

    'ListView' imported but unused

--- opps/core/views.py
+++ opps/core/views.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from opps.core.models.article import Post
