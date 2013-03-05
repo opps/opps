b47e3677120b9b2d64d38b48b0382dc7986a1b82
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 15:23:25 -0300

    Remove installed app on init opps core

--- opps/core/__init__.py
+++ opps/core/__init__.py
@@ -1,16 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 
 
 
 trans_app_label = _('Opps')
-settings.INSTALLED_APPS += ('opps.article',
-        'opps.image',
-        'opps.channel',
-        'opps.source',
-        'redactor',
-        'tagging',)
-
-settings.REDACTOR_OPTIONS = {'lang': 'en'}
-settings.REDACTOR_UPLOAD = 'uploads/'
