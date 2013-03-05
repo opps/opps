67e0765e3bc98720fbda5febffdd5d4c3b9865ef
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 15:24:44 -0300

    Create basic url on opps article

---
+++ opps/article/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+from django.conf.urls.defaults import patterns, url
+from django.conf.urls import include
+
+from opps.article.views import OppsDetail
+
+
+
+urlpatterns = patterns('',
+        url(r'^redactor/', include('redactor.urls')),
+        url(r'^(?P<channel__slug_name>[0-9A-Za-z-_.//]+)/(?P<slug>[\w-]+)$',
+            OppsDetail.as_view(), name='home'),
+)
