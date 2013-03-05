bb7e196ae1d4d5b4f9541da87e7d0c06c289e3af
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 02:12:18 -0300

    Fix pep8 in 72 characters on urls core

--- opps/core/urls.py
+++ opps/core/urls.py
@@ -10,5 +10,6 @@ from opps.core.views import OppsDetail
 
 urlpatterns = patterns('',
         url(r'^redactor/', include('redactor.urls')),
-        url(r'^(?P<channel__slug_name>[0-9A-Za-z-_.//]+)/(?P<slug>[\w-]+)$', OppsDetail.as_view(), name='home'),
+        url(r'^(?P<channel__slug_name>[0-9A-Za-z-_.//]+)/(?P<slug>[\w-]+)$',
+            OppsDetail.as_view(), name='home'),
 )
