3ddcffaf145d1fe808523c523a98ec1934f465ce
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 14:38:47 -0300

    isolate image application, create opps image

---
+++ opps/image/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from opps.core.models import Publishable
+from opps.core.models import Source
+
+
+
+class Image(Publishable):
+
+    title = models.CharField(_(u"Title"), max_length=140)
+    slug = models.SlugField(_(u"Slug"), max_length=150, blank=True)
+    image = models.ImageField(upload_to="uploads/")
+    description = models.CharField(_(u"Description"), max_length=255,
+            null=True, blank=True)
+
+    source = models.ForeignKey(Source, null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
