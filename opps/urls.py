9aea2ffd1482bce800c6790a4d390da81a85e0f0
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 01:46:48 -0300

    fix entre url base

--- opps/urls.py
+++ opps/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls import include
 
 
 urlpatterns = patterns('',
-        url(r'^$', include('opps.core.urls')),
+        url(r'', include('opps.core.urls', namespace='opps_core')),
 )
