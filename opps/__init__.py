a9f8c78fb417626fde15528530a7c8af7f0682a0
Guilherme Gondim <semente@taurinus.org>
2013-03-06 11:17:55 -0300

    Update __init__.py
    Fix license information

--- opps/__init__.py
+++ opps/__init__.py
@@ -10,5 +10,5 @@ __description__ = u"Opps CMS websites magazines and high-traffic"
 __author__ = u"Thiago Avelino"
 __credits__ = []
 __email__ = u"opps-developers@googlegroups.com"
-__license__ = u"BSD"
+__license__ = u"MIT License"
 __copyright__ = u"Copyright 2013, YACOWS"
