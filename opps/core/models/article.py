8f7ba327dd73fb4293cd92ebb49c04e6555b576d
Guilherme Gondim <semente@taurinus.org>
2013-03-06 17:36:34 -0300

    Use the form 'appname.Modelname' in foreign key relationships

--- opps/core/models/article.py
+++ opps/core/models/article.py
@@ -2,13 +2,9 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from opps.core.models import Publishable
-from opps.channel.models import Channel
-from opps.image.models import Image
-from opps.source.models import Source
-
 from tagging.fields import TagField
 
+from opps.core.models import Publishable
 
 
 class Article(Publishable):
@@ -19,15 +15,15 @@ class Article(Publishable):
     short_title = models.CharField(_(u"Short title"), max_length=140,
             blank=False, null=True)
     headline = models.TextField(_(u"Headline"), blank=True)
-    channel = models.ForeignKey(Channel, verbose_name=_(u"Channel"))
+    channel = models.ForeignKey('channel.Channel', verbose_name=_(u"Channel"))
 
     content = models.TextField(_(u"Content"))
 
-    main_image = models.ForeignKey(Image, verbose_name=_(u'Main Image'),
+    main_image = models.ForeignKey('image.Image', verbose_name=_(u'Main Image'),
             blank=False, null=True, on_delete=models.SET_NULL,
             related_name='article_main_image')
 
-    sources = models.ManyToManyField(Source, null=True, blank=True,
+    sources = models.ManyToManyField('source.Source', null=True, blank=True,
             related_name='post_sources', through='PostSource')
 
     tags = TagField(null=True, verbose_name=_(u"Tags"))
