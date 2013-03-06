d3bcd6426bc323a876ffab6ac46fe117f9e5ab13
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 21:24:43 -0300

    Remove django installed apps init opps

--- opps/__init__.py
+++ opps/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf import settings
 
 VERSION = (0, 1, 2)
 
@@ -13,13 +12,3 @@ __credits__ = []
 __email__ = u"opps-developers@googlegroups.com"
 __license__ = u"BSD"
 __copyright__ = u"Copyright 2013, YACOWS"
-
-settings.INSTALLED_APPS += ('opps.article',
-        'opps.image',
-        'opps.channel',
-        'opps.source',
-        'redactor',
-        'tagging',)
-
-settings.REDACTOR_OPTIONS = {'lang': 'en'}
-settings.REDACTOR_UPLOAD = 'uploads/'
