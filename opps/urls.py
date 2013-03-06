818b66b66be833276944d0442ad5b20d3c9c4e08
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 21:00:49 -0300

    fix regex urls, remove end url in include article

--- opps/urls.py
+++ opps/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls import include
 
 
 urlpatterns = patterns('',
-        url(r'^$', include('opps.article.urls', namespace='opps_article')),
+        url(r'^', include('opps.article.urls', namespace='opps_article')),
 )
