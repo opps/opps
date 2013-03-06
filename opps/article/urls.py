1e73839300d08b57b214abc2dd044be52806c8fa
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-05 23:18:37 -0300

    add oppslist on articles urls

--- opps/article/urls.py
+++ opps/article/urls.py
@@ -4,12 +4,14 @@
 from django.conf.urls.defaults import patterns, url
 from django.conf.urls import include
 
-from opps.article.views import OppsDetail
+from opps.article.views import OppsDetail, OppsList
 
 
 
 urlpatterns = patterns('',
         url(r'^redactor/', include('redactor.urls')),
         url(r'(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-            OppsDetail.as_view(), name='home'),
+            OppsDetail.as_view(), name='open'),
+        url(r'(?P<channel__long_slug>[\w//-]+)$',
+            OppsList.as_view(), name='channel'),
 )
