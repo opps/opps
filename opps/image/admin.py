b4f88006e5389519dca39d8682fd1b7dfb3d1426
André Melo <algmelo@gmail.com>
2013-03-07 18:44:29 -0300

    Fix image admin.

--- opps/image/admin.py
+++ opps/image/admin.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 from opps.image.models import Image
 
 
 class ImagesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ['title', 'source', 'date_available', 'published']
+    list_filter = ['date_available', 'published', 'source']
+    search_fields = ['title',]
+    raw_id_fields = ['source',]
     exclude = ('user',)
+
+
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('site', 'title', 'slug', 'image')}),
+        (_(u'Content'), {
+            'fields': ('description', 'source')}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available')}),
+    )
+    
 
     def save_model(self, request, obj, form, change):
         try:
