f23bbfa6d0361024e77590704fcff914227ca1cf
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-08 19:29:10 -0300

    Add urls to home page (/)

--- opps/article/urls.py
+++ opps/article/urls.py
@@ -10,8 +10,9 @@ from opps.article.views import OppsDetail, OppsList
 
 urlpatterns = patterns('',
         url(r'^redactor/', include('redactor.urls')),
-        url(r'(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
+        url(r'^$', OppsList.as_view(), name='home'),
+        url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
             OppsDetail.as_view(), name='open'),
-        url(r'(?P<channel__long_slug>[\w//-]+)$',
+        url(r'^(?P<channel__long_slug>[\w//-]+)$',
             OppsList.as_view(), name='channel'),
 )
