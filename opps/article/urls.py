6c80cf74714f79abd35a11075d06c84011d37a44
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-09 02:23:01 -0300

    Recover regex url open article and channel (\w)

--- opps/article/urls.py
+++ opps/article/urls.py
@@ -10,8 +10,8 @@ from opps.article.views import OppsDetail, OppsList
 urlpatterns = patterns('',
         url(r'^redactor/', include('redactor.urls')),
         url(r'^$', OppsList.as_view(), name='home'),
-        url(r'^(?P<channel__long_slug>.*)/(?P<slug>[\w-]+)$',
+        url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
             OppsDetail.as_view(), name='open'),
-        url(r'^(?P<channel__long_slug>.*)$',
+        url(r'^(?P<channel__long_slug>[\w//-]+)$',
             OppsList.as_view(), name='channel'),
 )
