c97236b5857ba538adaf73508e4843e56b5d12d9
Davi Campos <arlocm@gmail.com>
2013-03-11 23:14:34 -0300

    Changing datetime.now to timezone.now

--- opps/core/models/publishable.py
+++ opps/core/models/publishable.py
@@ -3,16 +3,15 @@ from django.db import models
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.sites.models import Site
+from django.utils import timezone
 
 from opps.core.models.date import Date
-
-from datetime import datetime
 
 
 class PublishableManager(models.Manager):
     def all_published(self):
         return super(PublishableManager, self).get_query_set().filter(
-            date_available__lte=datetime.now(), published=True)
+            date_available__lte=timezone.now(), published=True)
 
 
 class Publishable(Date):
@@ -20,7 +19,7 @@ class Publishable(Date):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     site = models.ForeignKey(Site, default=0)
     date_available = models.DateTimeField(_(u"Date available"),
-                                          default=datetime.now, null=True)
+                                          default=timezone.now, null=True)
     published = models.BooleanField(_(u"Published"), default=False)
 
     objects = PublishableManager()
@@ -29,5 +28,4 @@ class Publishable(Date):
         abstract = True
 
     def is_published(self):
-        return self.published and \
-            self.date_available.replace(tzinfo=None) <= datetime.now()
+        return self.published and self.date_available <= timezone.now()
