510c980f7cdb961fe88efcb1aa8bf1ff3ed51a78
Davi Campos <arlocm@gmail.com>
2013-03-11 22:52:01 -0300

    Adding urls to pep8

--- opps/article/urls.py
+++ opps/article/urls.py
@@ -6,11 +6,12 @@ from django.conf.urls import patterns, url, include
 from opps.article.views import OppsDetail, OppsList
 
 
-urlpatterns = patterns('',
-        url(r'^redactor/', include('redactor.urls')),
-        url(r'^$', OppsList.as_view(), name='home'),
-        url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-            OppsDetail.as_view(), name='open'),
-        url(r'^(?P<channel__long_slug>[\w//-]+)$',
-            OppsList.as_view(), name='channel'),
+urlpatterns = patterns(
+    '',
+    url(r'^redactor/', include('redactor.urls')),
+    url(r'^$', OppsList.as_view(), name='home'),
+    url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
+        OppsDetail.as_view(), name='open'),
+    url(r'^(?P<channel__long_slug>[\w//-]+)$', OppsList.as_view(),
+        name='channel'),
 )
--- opps/urls.py
+++ opps/urls.py
@@ -4,8 +4,8 @@
 from django.conf.urls import patterns, url, include
 
 
-
-urlpatterns = patterns('',
-        url(r'^', include('opps.article.urls', namespace='articles',
-            app_name='articles')),
+urlpatterns = patterns(
+    '',
+    url(r'^', include('opps.article.urls', namespace='articles',
+                      app_name='articles')),
 )
