1caa4f9e4a752526084f46ce4440b51523cd5bda
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-13 17:19:58 -0300

    Add article box on channel admin

--- opps/channel/admin.py
+++ opps/channel/admin.py
@@ -14,12 +14,12 @@ class ChannelAdmin(admin.ModelAdmin):
     list_filter = ['date_available', 'published', 'homepage', 'channel']
     search_fields = ['name']
     exclude = ('user', 'long_slug')
-    raw_id_fields = ['channel']
+    raw_id_fields = ['channel', 'articlebox']
 
     fieldsets = (
         (_(u'Identification'), {
             'fields': ('site', 'channel', 'name', 'slug', 'description',
-                       'position', 'homepage')}),
+                       'position', 'homepage', 'articlebox')}),
         (_(u'Publication'), {
             'classes': ('extrapretty'),
             'fields': ('published', 'date_available')}),
