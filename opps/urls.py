020707907788ada5953acce7a86ad4402d8511bb
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-13 16:07:25 -0300

    Add redactor urls in opps urls (fix issue #15)

--- opps/urls.py
+++ opps/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns(
     '',
+    url(r'^redactor/', include('redactor.urls')),
     url(r'^', include('opps.article.urls', namespace='articles',
                       app_name='articles')),
 )
