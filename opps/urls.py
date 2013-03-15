9ac8ad6591ee7fb193e267925dd1a8bc821f4551
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-15 15:04:17 -0300

    Add url entry sitemap xml in opps urls

--- opps/urls.py
+++ opps/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns(
     '',
     url(r'^redactor/', include('redactor.urls')),
+    url(r'^sitemap.xml', include('static_sitemaps.urls')),
     url(r'^', include('opps.article.urls', namespace='articles',
                       app_name='articles')),
 )
