737c5cac8658c01110e84a19a63c46ea32578003
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-18 19:01:28 -0300

    recover init opps on installed apps to opps.core

--- opps/__init__.py
+++ opps/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 import pkg_resources
 
 
@@ -19,36 +18,3 @@ __credits__ = []
 __email__ = u"opps-developers@googlegroups.com"
 __license__ = u"MIT License"
 __copyright__ = u"Copyright 2013, YACOWS"
-
-
-settings.INSTALLED_APPS += (
-    'opps.article',
-    'opps.image',
-    'opps.channel',
-    'opps.source',
-    'django.contrib.redirects',
-    'django_thumbor',
-    'googl',
-    'redactor',
-    'static_sitemaps',
-    'tagging',)
-
-settings.MIDDLEWARE_CLASSES += (
-    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',)
-
-# Opps
-getattr(settings, 'OPPS_SHORT', 'googl')
-getattr(settings, 'OPPS_SHORT_URL', 'googl.short.GooglUrlShort')
-
-# Sitemap
-if not hasattr(settings, 'STATICSITEMAPS_ROOT_SITEMAP'):
-    settings.STATICSITEMAPS_ROOT_SITEMAP = 'opps.sitemaps.feed.sitemaps'
-
-# redactor
-getattr(settings, 'REDACTOR_OPTIONS', {'lang': 'en'})
-getattr(settings, 'REDACTOR_UPLOAD', 'uploads/')
-
-# thumbor
-getattr(settings, 'THUMBOR_SERVER', 'http://localhost:8888')
-getattr(settings, 'THUMBOR_MEDIA_URL', 'http://localhost:8000/media')
-getattr(settings, 'THUMBOR_SECURITY_KEY', '')
--- opps/core/__init__.py
+++ opps/core/__init__.py
@@ -1,4 +1,38 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
 
 trans_app_label = _('Core')
+
+settings.INSTALLED_APPS += (
+    'opps.article',
+    'opps.image',
+    'opps.channel',
+    'opps.source',
+    'django.contrib.redirects',
+    'django_thumbor',
+    'googl',
+    'redactor',
+    'static_sitemaps',
+    'tagging',)
+
+settings.MIDDLEWARE_CLASSES += (
+    'django.contrib.redirects.middleware.RedirectFallbackMiddleware',)
+
+# Opps
+getattr(settings, 'OPPS_SHORT', 'googl')
+getattr(settings, 'OPPS_SHORT_URL', 'googl.short.GooglUrlShort')
+
+# Sitemap
+if not hasattr(settings, 'STATICSITEMAPS_ROOT_SITEMAP'):
+    settings.STATICSITEMAPS_ROOT_SITEMAP = 'opps.sitemaps.feed.sitemaps'
+
+# redactor
+getattr(settings, 'REDACTOR_OPTIONS', {'lang': 'en'})
+getattr(settings, 'REDACTOR_UPLOAD', 'uploads/')
+
+# thumbor
+getattr(settings, 'THUMBOR_SERVER', 'http://localhost:8888')
+getattr(settings, 'THUMBOR_MEDIA_URL', 'http://localhost:8000/media')
+getattr(settings, 'THUMBOR_SECURITY_KEY', '')
