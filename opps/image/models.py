cab837bac3c848a686d4bcf2bd5851a67fff65e7
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-20 18:34:52 -0300

    fix spelling on get file path

--- opps/image/models.py
+++ opps/image/models.py
@@ -15,8 +15,8 @@ def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "{0}-{1}.{2}".format(uuid.uuid4(), instance.slug, ext)
     d = datetime.now()
-    folter = "images/{0}".format(d.strftime("%Y/%m/%d/"))
-    return os.path.join(folter, filename)
+    folder = "images/{0}".format(d.strftime("%Y/%m/%d/"))
+    return os.path.join(folder, filename)
 
 
 class Image(Publishable):
