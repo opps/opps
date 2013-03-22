899e3c9f81a43dcb94e290ce0a86f128bd94effd
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-22 13:19:20 -0300

    Apply filter channel published on menu list (channel context processors)

--- opps/channel/context_processors.py
+++ opps/channel/context_processors.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from django.utils import timezone
 from .models import Channel
 
 
 def channel_context(request):
-    return {'opps_menu': Channel.objects.all()}
+    """ Channel context processors
+    """
+    opps_menu = Channel.objects.filter(date_available__lte=timezone.now(),
+                                       published=True)
+
+    return {'opps_menu': opps_menu}
