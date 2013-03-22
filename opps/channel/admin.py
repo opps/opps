e6da923a2abe11604e995b3acf1e3cb068642781
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-22 12:56:05 -0300

    fix channel admin (django) field channel to parent

--- opps/channel/admin.py
+++ opps/channel/admin.py
@@ -9,16 +9,16 @@ from opps.channel.utils import generate_long_slug
 
 class ChannelAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-    list_display = ['name', 'channel', 'date_available', 'homepage',
+    list_display = ['name', 'parent', 'date_available', 'homepage',
                     'position', 'published']
-    list_filter = ['date_available', 'published', 'homepage', 'channel']
+    list_filter = ['date_available', 'published', 'homepage', 'parent']
     search_fields = ['name']
     exclude = ('user', 'long_slug')
-    raw_id_fields = ['channel']
+    raw_id_fields = ['parent']
 
     fieldsets = (
         (_(u'Identification'), {
-            'fields': ('site', 'channel', 'name', 'slug', 'description',
+            'fields': ('site', 'parent', 'name', 'slug', 'description',
                        'position', 'homepage',)}),
         (_(u'Publication'), {
             'classes': ('extrapretty'),
@@ -26,7 +26,7 @@ class ChannelAdmin(admin.ModelAdmin):
     )
 
     def save_model(self, request, obj, form, change):
-        obj.long_slug = generate_long_slug(obj.channel, obj.slug,
+        obj.long_slug = generate_long_slug(obj.parent, obj.slug,
                                            obj.site.domain)
         try:
             if obj.user:
