da4bf02a309e1ce572e3b9ac5599861bd22400e9
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-26 01:41:49 -0300

    Site site on context processors opps_menu

--- opps/channels/context_processors.py
+++ opps/channels/context_processors.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.utils import timezone
+from django.contrib.sites.models import get_current_site
+
 from .models import Channel
 
 
 def channel_context(request):
     """ Channel context processors
     """
-    opps_menu = Channel.objects.filter(date_available__lte=timezone.now(),
+    site = get_current_site(request)
+    opps_menu = Channel.objects.filter(site=site,
+                                       date_available__lte=timezone.now(),
                                        published=True)
 
     return {'opps_menu': opps_menu}
