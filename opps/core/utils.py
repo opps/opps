23fe1fa1a898e3db313106b47d21e48f44600421
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-29 12:58:59 -0300

    create core utils, add set context data use on generic views (Opps)

---
+++ opps/core/utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+
+
+def set_context_data(self, SUPER, **kwargs):
+    context = super(SUPER, self).get_context_data(**kwargs)
+    if len(self.article) >= 1:
+        article = self.article.get()
+        context['opps_channel'] = article.channel
+        context['opps_channel_conf'] = settings.OPPS_CHANNEL_CONF\
+                .get(article.channel.slug, '')
+    return context
