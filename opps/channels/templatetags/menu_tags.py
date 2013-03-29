64c4027f161f6bca2aeb9ee18849862d7d4cc695
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-03-28 21:26:16 -0300

    Create channel tamplate tags ofKey

---
+++ opps/channels/templatetags/menu_tags.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django import template
+
+
+register = template.Library()
+
+@register.filter
+def ofKey(value, arg):
+    if value:
+        return value.get(arg)
+    return ""
