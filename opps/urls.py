e84326c3bc915ddd2f8baf699a800ef0bae30d5c
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-01 16:34:39 -0300

    include sitemaps url on opps urls

--- opps/urls.py
+++ opps/urls.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 from django.conf.urls import patterns, url, include
 
 
 urlpatterns = patterns(
     '',
     url(r'^redactor/', include('redactor.urls')),
-    url(r'^sitemap.xml', include('static_sitemaps.urls')),
+
+    url(r'^sitemap', include('opps.sitemaps.urls')),
+
     url(r'^', include('opps.articles.urls', namespace='articles',
                       app_name='articles')),
 )
