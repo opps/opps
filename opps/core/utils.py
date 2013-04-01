a8143ada14cb24c6150521cf7c0e1f631a4efa52
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-01 20:49:10 -0300

    Fix bug, get multiobj on core utils set context data

--- opps/core/utils.py
+++ opps/core/utils.py
@@ -5,7 +5,7 @@ from django.conf import settings
 def set_context_data(self, SUPER, **kwargs):
     context = super(SUPER, self).get_context_data(**kwargs)
     if len(self.article) >= 1:
-        article = self.article.get()
+        article = self.article[0]
         context['opps_channel'] = article.channel
         context['opps_channel_conf'] = settings.OPPS_CHANNEL_CONF\
                 .get(article.channel.slug, '')
