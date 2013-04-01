ba03cd890105e391ea748b4adb5e74ad98bdfe77
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-01 16:37:35 -0300

    Add django admin url on opps urls

--- opps/urls.py
+++ opps/urls.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url, include
+from django.contrib import admin
 
 
 urlpatterns = patterns(
     '',
+    url(r'^admin/', include(admin.site.urls)),
+
     url(r'^redactor/', include('redactor.urls')),
 
     url(r'^sitemap', include('opps.sitemaps.urls')),
-
     url(r'^', include('opps.articles.urls', namespace='articles',
                       app_name='articles')),
 )
