e6f19cc58f32b855fc1f71086dac0ad56b697ed3
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-02 01:06:44 -0300

    Add cache on article page (via url)

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
 from django.conf.urls import patterns, url
+from django.views.decorators.cache import cache_page
 
 from .views import OppsDetail, OppsList, Search
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', OppsList.as_view(), name='home'),
+    url(r'^$', cache_page(60 * 2)(OppsList.as_view()), name='home'),
     url(r'^search/', Search(), name='search'),
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-        OppsDetail.as_view(), name='open'),
-    url(r'^(?P<channel__long_slug>[\w\b//-]+)/$', OppsList.as_view(),
-        name='channel'),
+        cache_page(60 * 15)(OppsDetail.as_view()), name='open'),
+    url(r'^(?P<channel__long_slug>[\w\b//-]+)/$',
+        cache_page(60 * 2)(OppsList.as_view()), name='channel'),
 )
