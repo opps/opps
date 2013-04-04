bbab9667ba185208b863efbe171660ae81608c3d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-04 01:42:01 -0300

    admin dynamic box

---
+++ opps/boxes/admin.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import QuerySet, DynamicBox
+from opps.core.admin import PublishableAdmin
+
+
+class QuerySetAdmin(PublishableAdmin):
+    prepopulated_fields = {"slug": ["name"]}
+    list_display = ['name', 'date_available', 'published']
+    list_filter = ['date_available', 'published']
+    exclude = ('user',)
+
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('site', 'name', 'slug')}),
+        (_(u'Rules'), {
+            'fields': ('model',)}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available')}),
+    )
+
+
+class DynamicBoxAdmin(PublishableAdmin):
+    prepopulated_fields = {"slug": ["name"]}
+    list_display = ['name', 'date_available', 'published']
+    list_filter = ['date_available', 'published']
+    exclude = ('user',)
+    raw_id_fields = ['channel', 'article', 'dynamicqueryset']
+
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('site', 'name', 'slug')}),
+        (_(u'Relationships'), {
+            'fields': ('channel', 'article', 'dynamicqueryset')}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available')}),
+    )
+
+
+admin.site.register(QuerySet, QuerySetAdmin)
+admin.site.register(DynamicBox, DynamicBoxAdmin)
