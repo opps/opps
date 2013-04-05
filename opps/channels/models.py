45824772bdc8ad2228f0aeeff4ba1dd7f9f8fbef
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-05 11:24:08 -0300

    fix typo field name on channel position to order

--- opps/channels/models.py
+++ opps/channels/models.py
@@ -29,7 +29,7 @@ class Channel(MPTTModel, Publishable):
                                    max_length=255, null=True, blank=True)
     show_in_menu = models.BooleanField(_(u"Show in menu?"), default=False)
     homepage = models.BooleanField(_(u"Is home page?"), default=False)
-    position = models.IntegerField(_(u"Position"), default=0)
+    order = models.IntegerField(_(u"Order"), default=0)
     parent = TreeForeignKey('self', related_name='subchannel',
                             null=True, blank=True)
 
