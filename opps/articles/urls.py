8e6eb9e8e8acf198c2bdbe9e0aacf6ef2adc7018
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-08 18:39:27 -0300

    change OppsList to PostList on urls

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -3,15 +3,15 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 
-from .views import OppsDetail, OppsList, Search
+from .views import OppsDetail, PostList, Search
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', cache_page(60 * 2)(OppsList.as_view()), name='home'),
+    url(r'^$', cache_page(60 * 2)(PostList.as_view()), name='home'),
     url(r'^search/', Search(), name='search'),
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(OppsDetail.as_view()), name='open'),
     url(r'^(?P<channel__long_slug>[\w\b//-]+)/$',
-        cache_page(60 * 2)(OppsList.as_view()), name='channel'),
+        cache_page(60 * 2)(PostList.as_view()), name='channel'),
 )
