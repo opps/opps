32d95aaf5b9b808ef43cf5e4d7ed5e554eb4632f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-08 20:29:33 -0300

    Create basic urls albuns /album/slug /album/channel/slug

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -3,13 +3,21 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 
-from .views import PostDetail, PostList, Search
+from .views import PostDetail, PostList, AlbumList, AlbumDetail, Search
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', cache_page(60 * 2)(PostList.as_view()), name='home'),
     url(r'^search/', Search(), name='search'),
+
+    # ALBUM
+    url(r'^album/(?P<channel__long_slug>[\w\b//-]+)/$',
+        cache_page(60 * 2)(AlbumList.as_view()), name='album_channel'),
+    url(r'^album/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
+        cache_page(60 * 15)(AlbumDetail.as_view()), name='album_open'),
+
+    # POST
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(PostDetail.as_view()), name='open'),
     url(r'^(?P<channel__long_slug>[\w\b//-]+)/$',
