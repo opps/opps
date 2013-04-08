6b9c3666ae4ef52b60113cca53c2df18aafbef86
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-08 18:38:21 -0300

    Create PostList extend OppsList OppsList base ListView

--- opps/articles/views.py
+++ opps/articles/views.py
@@ -26,36 +26,11 @@ class OppsList(ListView):
 
     @property
     def template_name(self):
-        domain_folder = 'channels'
+        domain_folder = self.type
         if self.site.id > 1:
-            domain_folder = "{0}/channels".format(self.site)
+            domain_folder = "{0}/{1}".format(self.site, self.type)
 
         return '{0}/{1}.html'.format(domain_folder, self.long_slug)
-
-    @property
-    def queryset(self):
-        self.site = get_current_site(self.request)
-        self.long_slug = None
-        if not self.kwargs.get('channel__long_slug'):
-            self.article = Post.objects.filter(channel__homepage=True,
-                                               site=self.site,
-                                               date_available__lte=timezone.
-                                               now(),
-                                               published=True).all()
-            homepage = Channel.objects.get_homepage(site=self.site)
-            if homepage:
-                self.long_slug = homepage.long_slug
-            return self.article
-        self.long_slug = self.kwargs['channel__long_slug']
-        self.channel = get_object_or_404(Channel, site=self.site,
-                                         long_slug=self.long_slug,
-                                         date_available__lte=timezone.now(),
-                                         published=True)
-        self.article = Post.objects.filter(site=self.site,
-                                           channel=self.channel,
-                                           date_available__lte=timezone.now(),
-                                           published=True).all()
-        return self.article
 
 
 class OppsDetail(DetailView):
@@ -89,6 +64,34 @@ class OppsDetail(DetailView):
         self.article = Post.objects.filter(site=self.site,
                                            channel__long_slug=self.long_slug,
                                            slug=self.kwargs['slug'],
+                                           date_available__lte=timezone.now(),
+                                           published=True).all()
+        return self.article
+
+
+class PostList(OppsList):
+    @property
+    def queryset(self):
+        self.site = get_current_site(self.request)
+        self.type = "channels"
+        self.long_slug = None
+        if not self.kwargs.get('channel__long_slug'):
+            self.article = Post.objects.filter(channel__homepage=True,
+                                               site=self.site,
+                                               date_available__lte=timezone.
+                                               now(),
+                                               published=True).all()
+            homepage = Channel.objects.get_homepage(site=self.site)
+            if homepage:
+                self.long_slug = homepage.long_slug
+            return self.article
+        self.long_slug = self.kwargs['channel__long_slug']
+        self.channel = get_object_or_404(Channel, site=self.site,
+                                         long_slug=self.long_slug,
+                                         date_available__lte=timezone.now(),
+                                         published=True)
+        self.article = Post.objects.filter(site=self.site,
+                                           channel=self.channel,
                                            date_available__lte=timezone.now(),
                                            published=True).all()
         return self.article
