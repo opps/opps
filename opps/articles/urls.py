024b91aa16e6e0f95c27279244521756b7c90203
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-08 18:55:16 -0300

    Fix urls details articles

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -3,7 +3,7 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 
-from .views import OppsDetail, PostList, Search
+from .views import PostDetail, PostList, Search
 
 
 urlpatterns = patterns(
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     url(r'^$', cache_page(60 * 2)(PostList.as_view()), name='home'),
     url(r'^search/', Search(), name='search'),
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
-        cache_page(60 * 15)(OppsDetail.as_view()), name='open'),
+        cache_page(60 * 15)(PostDetail.as_view()), name='open'),
     url(r'^(?P<channel__long_slug>[\w\b//-]+)/$',
         cache_page(60 * 2)(PostList.as_view()), name='channel'),
 )
