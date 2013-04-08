dfe1206bc748462a283ed644827355411a98e270
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-08 18:58:47 -0300

    Move base views to opps core application

--- opps/articles/views.py
+++ opps/articles/views.py
@@ -1,57 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from django.contrib.sites.models import get_current_site
 from django.core.paginator import Paginator, InvalidPage
-from django.views.generic.detail import DetailView
-from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
-from django import template
 from django.utils import timezone
 from django.http import Http404
 
 from haystack.views import SearchView
 
 from .models import Post
-from .utils import set_context_data
 from opps.channels.models import Channel
-
-
-class OppsList(ListView):
-
-    context_object_name = "context"
-
-    def get_context_data(self, **kwargs):
-        return set_context_data(self, OppsList, **kwargs)
-
-    @property
-    def template_name(self):
-        domain_folder = self.type
-        if self.site.id > 1:
-            domain_folder = "{0}/{1}".format(self.site, self.type)
-
-        return '{0}/{1}.html'.format(domain_folder, self.long_slug)
-
-
-class OppsDetail(DetailView):
-
-    context_object_name = "context"
-
-    def get_context_data(self, **kwargs):
-        return set_context_data(self, OppsDetail, **kwargs)
-
-    @property
-    def template_name(self):
-        domain_folder = self.type
-        if self.site.id > 1:
-            domain_folder = "{0}/{1}".format(self.site, self.type)
-        try:
-            _template = '{0}/{1}/{2}.html'.format(
-                domain_folder, self.long_slug, self.article.get().slug)
-            template.loader.get_template(_template)
-        except template.TemplateDoesNotExist:
-            _template = '{0}/{1}.html'.format(domain_folder, self.long_slug)
-        return _template
+from opps.core.models import OppsDetail, OppsList
 
 
 class PostDetail(OppsDetail):
---
+++ opps/core/views.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django import template
+
+from .utils import set_context_data
+
+
+class OppsList(ListView):
+
+    context_object_name = "context"
+
+    def get_context_data(self, **kwargs):
+        return set_context_data(self, OppsList, **kwargs)
+
+    @property
+    def template_name(self):
+        domain_folder = self.type
+        if self.site.id > 1:
+            domain_folder = "{0}/{1}".format(self.site, self.type)
+
+        return '{0}/{1}.html'.format(domain_folder, self.long_slug)
+
+
+class OppsDetail(DetailView):
+
+    context_object_name = "context"
+
+    def get_context_data(self, **kwargs):
+        return set_context_data(self, OppsDetail, **kwargs)
+
+    @property
+    def template_name(self):
+        domain_folder = self.type
+        if self.site.id > 1:
+            domain_folder = "{0}/{1}".format(self.site, self.type)
+        try:
+            _template = '{0}/{1}/{2}.html'.format(
+                domain_folder, self.long_slug, self.article.get().slug)
+            template.loader.get_template(_template)
+        except template.TemplateDoesNotExist:
+            _template = '{0}/{1}.html'.format(domain_folder, self.long_slug)
+        return _template
+
+
