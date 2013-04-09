79928051b481f9e19b45c8eebcf8ae2ff229b342
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-09 15:15:18 -0300

    Fix OPPS_APPS, get object_name in dropdawn

--- opps/boxes/models.py
+++ opps/boxes/models.py
@@ -8,20 +8,10 @@ from django.utils.translation import ugettext_lazy as _
 from opps.core.models import Publishable, BaseBox
 
 
-"""
-from django.db.models import get_model
-model = get_model('myapp', 'modelA')
-model.objects.filter(**kwargs)
-
-(Pdb)  models.get_models()[15]._meta.local_fields[0].verbose_name
-u'ID'
-(Pdb)  models.get_models()[15]._meta.local_fields[0].name
-u'id'
-"""
-
 try:
-    OPPS_APPS = tuple([(app._meta.app_label, u"{0} - {1}".format(
-        app._meta.app_label, app._meta.object_name))
+    OPPS_APPS = tuple([(u"{0}.{1}".format(
+        app._meta.app_label, app._meta.object_name), u"{0} - {1}".format(
+            app._meta.app_label, app._meta.object_name))
         for app in models.get_models() if 'opps.' in app.__module__])
 except ImportError:
     OPPS_APPS = tuple([])
@@ -37,8 +27,9 @@ class QuerySet(Publishable):
     )
 
     model = models.CharField(_(u'Model'), max_length=150, choices=OPPS_APPS)
-    order = models.CharField(_('Order'), max_length=2, choices=(
-        ('-', 'DESC'), ('', 'ASC')))
+    limit = models.PositiveIntegerField(_(u'Limit'))
+    order = models.CharField(_('Order'), max_length=1, choices=(
+        ('-', 'DESC'), ('+', 'ASC')))
 
 
 class DynamicBox(BaseBox):
