d3d4f28e00323c3ebf3488f012b72fcb2695e120
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-10 17:55:31 -0300

    create url entry tag page refs #72

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -3,7 +3,8 @@
 from django.conf.urls import patterns, url
 from django.views.decorators.cache import cache_page
 
-from .views import PostDetail, PostList, AlbumList, AlbumDetail, Search
+from .views import PostDetail, PostList, AlbumList, AlbumDetail, TagList
+from .views import Search
 
 
 urlpatterns = patterns(
@@ -16,6 +17,10 @@ urlpatterns = patterns(
         cache_page(60 * 2)(AlbumList.as_view()), name='album_channel'),
     url(r'^album/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(AlbumDetail.as_view()), name='album_open'),
+
+    # TAGs
+    url(r'^tag/(?P<tag>[\w]+)$',
+        cache_page(60 * 2)(TagList.as_view()), name='tag_open'),
 
     # POST
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
