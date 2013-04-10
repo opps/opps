c2a6da6a0ade645b90b6ccf9c30061401609b68e
Bruno Rocha <rochacbruno@gmail.com>
2013-04-10 19:37:30 +0000

    smarter get_app_model using django.models.get_app

--- opps/core/utils.py
+++ opps/core/utils.py
@@ -1,11 +1,11 @@
 # coding: utf-8
 
-from django.db.models import get_models
+from django.db.models import get_models, get_app
 
 
 def get_app_model(appname, suffix=""):
     app_label = appname.split('.')[-1]
-    models = [model for model in get_models()
+    models = [model for model in get_models(get_app(app_label))
               if (model.__name__.endswith(suffix) or not suffix)
               and model._meta.app_label == app_label]
     return models and models[0]
