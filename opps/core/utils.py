7ea250acdddf18330aee3604eb5eade5ba53eb78
Bruno Rocha <rochacbruno@gmail.com>
2013-04-10 18:57:58 +0000

    Fix endswith bug in utils.get_app_model

--- opps/core/utils.py
+++ opps/core/utils.py
@@ -3,7 +3,7 @@
 from django.db.models import get_models
 
 
-def get_app_model(appname, suffix=None):
+def get_app_model(appname, suffix=""):
     app_label = appname.split('.')[-1]
     models = [model for model in get_models()
               if (model.__name__.endswith(suffix) or not suffix)
