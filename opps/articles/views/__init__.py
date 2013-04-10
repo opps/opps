90c6f187c36e5f8ec032c96283e53759ae131ecf
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-10 17:54:53 -0300

    Create taglist view on articles

--- opps/articles/views/__init__.py
+++ opps/articles/views/__init__.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.core.paginator import Paginator, InvalidPage
+from django.contrib.sites.models import get_current_site
+from django.utils import timezone
 from django.http import Http404
 
 from haystack.views import SearchView
 
-from opps.articles.models import Post, Album
+from opps.articles.models import Post, Album, Article
 from opps.articles.views.generic import OppsDetail, OppsList
 
 
@@ -27,6 +29,23 @@ class AlbumList(OppsList):
 class AlbumDetail(OppsDetail):
     obj = Album
     type = 'articles/album'
+
+
+class TagList(OppsList):
+    obj = Article
+    type = "tags"
+    template_name_suffix = '_tags'
+
+    @property
+    def queryset(self):
+        self.site = get_current_site(self.request)
+        self.long_slug = self.kwargs['tag']
+        self.article = self.obj.objects.filter(site=self.site,
+                                               tags__slug=self.long_slug,
+                                               date_available__lte=timezone.
+                                               now(),
+                                               published=True).all()
+        return self.article
 
 
 class Search(SearchView):
