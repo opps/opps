43cccd46d638023f32fd5d0432566d939f693dae
Ellison Leao <ellisonleao@gmail.com>
2013-04-11 19:17:00 +0000

    Added initial files

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-example
-=======
+Opps CMS Example
+================
 
-Example of using Opps
+## First things firsts
+
+Create a virtualenv environment ( if you don't know what this is read
+[this]("http://virtualenvwrapper.readthedocs.org/en/latest/")).
+
+After the virtual environment creation you should do:
+
+    $ pip install opps
+
+Trouble installing Opps? Go
+[here]("http://www.oppsproject.org/en/latest/installation.html")
+
+
+## Create a project
+
+    $ django-admin.py createproject mysite
+
+Since the Opps CMS is modularized with several apps, we need to add the
+base apps in the INSTALLED_APPS tuple: 
+
+    INSTALLED_APPS (
+		...
+		'opps.core',
+        'opps.articles',
+        'opps.channels',
+        'opps.images',
+        'opps.search',
+        'opps.sitemaps',
+        'opps.sources',
+    )
+
+Now you can run:
+
+	$ python manage.py syncdb
+	$ python manage.py migrate
+
+
+If you are having trouble to configure your project settings.py you can
+look at our settings.py example.
+
+And that's it! Don't forget to enable Django's admin to start adding
+content!
+
+Got any issues? Go to the [Opps Github
+Issues]('http://github.com/opps/opps/issues')
+
+
+
+
+
---
+++ example/settings.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_PATH, 'dev.db'),
+    }
+}
+
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+USE_TZ = False
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = '8=#&amp;5-4@j#54=!(nwo5q8$y9i%y5c384jnp6pf7@m*q&amp;#71xc1'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.request",
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
+    'opps.core',
+    'opps.boxes',
+    'opps.channels',
+    'opps.sources',
+    'opps.articles',
+    'opps.images',
+    'opps.sitemaps',
+
+    'south',
+    'taggit',
+)
+
+TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+STATIC_URL = '/static/'
+ROOT_URLCONF = 'example.urls'
