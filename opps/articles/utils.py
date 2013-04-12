92f5b3a7fa77e12ca3f9aa974d0069ee6396a793
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-12 20:28:13 -0300

    update articleboxes context, articles views set_context_data

--- opps/articles/utils.py
+++ opps/articles/utils.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
-from django.conf import settings
-
 from opps.articles.models import ArticleBox
 
 
 def set_context_data(self, SUPER, **kwargs):
     context = super(SUPER, self).get_context_data(**kwargs)
-    if len(self.article) >= 1:
-        article = self.article[0]
 
-        context['articleboxes'] = ArticleBox.objects.filter(
-            channel=article.channel)
-        if len(self.article) == 1:
-            context['articleboxes'] = context['articleboxes'].filter(
-                article=article)
+    context['articleboxes'] = ArticleBox.objects.filter(
+        channel__long_slug=self.long_slug)
+    if self.slug:
+        context['articleboxes'] = context['articleboxes'].filter(
+            article__slug=self.slug)
 
-        context['opps_channel'] = article.channel
-        context['opps_channel_conf'] = settings.OPPS_CHANNEL_CONF.get(
-            article.channel.slug, '')
     return context
