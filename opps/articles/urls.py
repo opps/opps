36d78719a6c5640315742bd4f2bef7dac60af44a
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-13 18:00:29 -0300

    Add feed url on all channel

--- opps/articles/urls.py
+++ opps/articles/urls.py
@@ -5,26 +5,31 @@ from django.views.decorators.cache import cache_page
 
 from .views import PostDetail, PostList, AlbumList, AlbumDetail, TagList
 from .views import Search
-from .views.feed import ArticleFeed
+from .views.feed import ArticleFeed, ChannelFeed
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', cache_page(60 * 2)(PostList.as_view()), name='home'),
-    url(r'^(RSS|FEED)$', cache_page(60 * 2)(ArticleFeed()), name='feed'),
+    url(r'^(rss|feed)$', cache_page(60 * 2)(ArticleFeed()), name='feed'),
     url(r'^search/', Search(), name='search'),
 
     # ALBUM
-    url(r'^album/(?P<channel__long_slug>[\w\b//-]+)/$',
-        cache_page(60 * 2)(AlbumList.as_view()), name='album_channel'),
+    url(r'^album/(?P<long_slug>[\w\b//-]+)/(rss|feed)$',
+        cache_page(60 * 2)(ChannelFeed(model='Album')),
+        name='album_feed'),
     url(r'^album/(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(AlbumDetail.as_view()), name='album_open'),
+    url(r'^album/(?P<channel__long_slug>[\w\b//-]+)/$',
+        cache_page(60 * 2)(AlbumList.as_view()), name='album_channel'),
 
     # TAGs
     url(r'^tag/(?P<tag>[\w//-]+)$',
         cache_page(60 * 2)(TagList.as_view()), name='tag_open'),
 
     # POST
+    url(r'^(?P<long_slug>[\w\b//-]+)/(rss|feed)$',
+        cache_page(60 * 2)(ChannelFeed(model='Post')), name='channel_feed'),
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)$',
         cache_page(60 * 15)(PostDetail.as_view()), name='open'),
     url(r'^(?P<channel__long_slug>[\w\b//-]+)/$',
