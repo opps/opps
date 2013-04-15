08bcf896f53f7cf29589ca3e6630fd78464d0872
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-15 19:09:53 -0300

    opps/flatpages/admin.py:7:1: F401 timezone imported but unused

--- opps/flatpages/admin.py
+++ opps/flatpages/admin.py
@@ -4,7 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.sites.models import Site
-from django.utils import timezone
 from django.conf import settings
 
 from redactor.widgets import RedactorEditor
