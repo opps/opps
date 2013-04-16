1ffdbcb9766136b0aee4be4f2f067fabf456e30f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-16 03:15:43 -0300

    Set developer version 0.1.7 start developer version on date 2013.04.16

--- opps/__init__.py
+++ opps/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 1, 6)
+VERSION = (0, 1, 7, 'dev')
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
