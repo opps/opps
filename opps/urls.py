29920a5fd4deacdc9762783f5604a610dd37080e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-16 11:20:51 -0300

    Add admin autodiscover on opps.urls

--- opps/urls.py
+++ opps/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 
+admin.autodiscover()
+
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
