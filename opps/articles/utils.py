d3f2b3b5798e39569ee5cccc20e7bf99b4783e4f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-18 18:49:04 -0300

    set context var albums on context data, articles utils

--- opps/articles/utils.py
+++ opps/articles/utils.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from opps.articles.models import ArticleBox
+from opps.articles.models import ArticleBox, Album
 
 
 def set_context_data(self, SUPER, **kwargs):
     context = super(SUPER, self).get_context_data(**kwargs)
 
+    context['albums'] = Album.objects.filter(
+        channel_long_slug=self.long_slug)[:self.limit]
     context['channel_long_slug'] = self.long_slug
     context['articleboxes'] = ArticleBox.objects.filter(
         channel__long_slug=self.long_slug)
