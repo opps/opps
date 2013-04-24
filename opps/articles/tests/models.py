98103ccdf6439a8b9a3454d3856bc03a35fb663d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-24 10:36:26 -0300

    split article test of article type

--- opps/articles/tests/models.py
+++ opps/articles/tests/models.py
@@ -2,7 +2,26 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 
-from opps.articles.models import Post
+from opps.articles.models import Article, Post
+
+
+class ArticleModelTest(TestCase):
+
+    fixtures = ['tests/initial_data.json']
+
+    def setUp(self):
+        self.article = Article.objects.get(id=1)
+
+    def test_child_class(self):
+        self.assertTrue(self.article.child_class)
+        self.assertEqual(self.article.child_class, 'Post')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.article.get_absolute_url(),
+                         u'/channel-01/test-post-application')
+        self.assertEqual(self.article.get_absolute_url(),
+                         "/{0}/{1}".format(self.article.channel.long_slug,
+                                           self.article.slug))
 
 
 class PostModelTest(TestCase):
@@ -22,15 +41,4 @@ class PostModelTest(TestCase):
         self.assertEqual(post[0].title, u'test post application')
         self.assertTrue(post[0].short_url)
 
-    def test_child_class(self):
 
-        self.assertTrue(self.post.child_class)
-        self.assertEqual(self.post.child_class, 'Post')
-
-    def test_get_absolute_url(self):
-
-        self.assertEqual(self.post.get_absolute_url(),
-                         u'/channel-01/test-post-application')
-        self.assertEqual(self.post.get_absolute_url(),
-                         "/{0}/{1}".format(self.post.channel.long_slug,
-                                           self.post.slug))
