b54023ad44e0f4e7ecc93ee61ce4580804867b26
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-24 09:41:52 -0300

    Fix test articles, reorganize

--- opps/articles/tests/models.py
+++ opps/articles/tests/models.py
@@ -9,17 +9,20 @@ class PostModelTest(TestCase):
 
     fixtures = ['tests/initial_data.json']
 
+    def setUP(self):
+        self.post = Post.objects.get(id=1)
+
     def test_basic_post_exist(self):
         post = Post.objects.all()
 
         self.assertTrue(post)
+        self.assertTrue(post[0], self.post)
         self.assertEqual(len(post), 1)
         self.assertEqual(post[0].slug, u'test-post-application')
         self.assertEqual(post[0].title, u'test post application')
         self.assertTrue(post[0].short_url)
 
     def test_child_class(self):
-        post = Post.objects.get(id=1)
 
-        self.assertTrue(post.child_class)
-        self.assertEqual(post.child_class, 'Post')
+        self.assertTrue(self.post.child_class)
+        self.assertEqual(self.post.child_class, 'Post')
