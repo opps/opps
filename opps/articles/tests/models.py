f0c53900a62159249240dedc486678d451932cb1
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-24 09:34:41 -0300

    Fix articles test, used test initial_data

--- opps/articles/tests/models.py
+++ opps/articles/tests/models.py
@@ -1,41 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.test import TestCase
-from django.contrib.sites.models import Site
-from django.contrib.auth import get_user_model
-from django.core.files import File
 
 from opps.articles.models import Post
-from opps.channels.models import Channel
-from opps.images.models import Image
 
 
 class PostModelTest(TestCase):
 
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create(username=u'test', password='test')
-        self.site = Site.objects.filter(name=u'example.com').get()
-        self.channel = Channel.objects.create(name=u'Home', slug=u'home',
-                                              description=u'home page',
-                                              site=self.site, user=self.user)
-
-        image = File(open("opps/__init__.py"), "test_file.png")
-        self.image = Image.objects.create(site=self.site, title='test',
-                                          image=image, user=self.user)
-
-        self.post = Post.objects.create(
-            title=u'Basic test',
-            slug=u'basic-test',
-            short_title=u'Write basict test for Article type Post',
-            content=u'This field is context',
-            main_image=self.image,
-            channel=self.channel,
-            user=self.user
-        )
+    fixtures = ['tests/initial_data.json']
 
     def test_basic_post_exist(self):
         post = Post.objects.all()
 
         self.assertTrue(post)
-        self.assertEqual(post[0], self.post)
+        self.assertEqual(len(post), 1)
+        self.assertEqual(post[0].slug, u'test-post-application')
+        self.assertEqual(post[0].title, u'test post application')
+        self.assertTrue(post[0].short_url)
