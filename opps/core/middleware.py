30ebea290aaf96b016263826c687bcc921e555f9
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-04-27 13:45:41 -0300

    create middleware Mobile Detection on core

--- opps/core/middleware.py
+++ opps/core/middleware.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from django.shortcuts import get_object_or_404
 from django.contrib.sites.models import Site
 from django.conf import settings
@@ -27,3 +28,33 @@ class DynamicSiteMiddleware(object):
         site = self.get_hosting(hosting)
 
         settings.SITE_ID = site.id
+
+
+class MobileDetectionMiddleware(object):
+
+    user_agents_test_match = (
+        "w3c ", "acs-", "alav", "alca", "amoi", "audi",
+        "avan", "benq", "bird", "blac", "blaz", "brew",
+        "cell", "cldc", "cmd-", "dang", "doco", "eric",
+        "hipt", "inno", "ipaq", "java", "jigs", "kddi",
+        "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-",
+        "maui", "maxo", "midp", "mits", "mmef", "mobi",
+        "mot-", "moto", "mwbp", "nec-", "newt", "noki",
+        "xda",  "palm", "pana", "pant", "phil", "play",
+        "port", "prox", "qwap", "sage", "sams", "sany",
+        "sch-", "sec-", "send", "seri", "sgh-", "shar",
+        "sie-", "siem", "smal", "smar", "sony", "sph-",
+        "symb", "t-mo", "teli", "tim-", "tosh", "tsm-",
+        "upg1", "upsi", "vk-v", "voda", "wap-", "wapa",
+        "wapi", "wapp", "wapr", "webc", "winw", "winw",
+        "xda-",)
+
+    user_agents_test_search = u"(?:%s)" % u'|'.join((
+        'up.browser', 'up.link', 'mmp', 'symbian', 'smartphone', 'midp',
+        'wap', 'phone', 'windows ce', 'pda', 'mobile', 'mini', 'palm',
+        'netfront', 'opera mobi',))
+
+    user_agents_exception_search = u"(?:%s)" % u'|'.join(('ipad',))
+
+    http_accept_regex = re.compile("application/vnd\.wap\.xhtml\+xml",
+                                   re.IGNORECASE)
