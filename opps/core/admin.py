65c5cbe156ea36343180a941c6a74d7931d95f57
Bruno Rocha <rochacbruno@gmail.com>
2013-04-28 04:10:42 -0300

    added apply_rules on core.admin to customize admin based in settings.py rules for apps

--- opps/core/admin.py
+++ opps/core/admin.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
 
 
 class PublishableAdmin(admin.ModelAdmin):
@@ -25,3 +26,43 @@ class PublishableAdmin(admin.ModelAdmin):
             obj.site = Site.objects.get(pk=settings.SITE_ID)
         obj.date_update = timezone.now()
         obj.save()
+
+
+def apply_rules(admin_class, rules):
+    """
+    To allow overrides of admin rules for opps apps
+    it uses the settings.py to load the values
+
+    example of use:
+
+    OPPS_ADMIN_RULES = getattr(settings, 'OPPS_ADMIN_RULES', {})
+    promo_admin_rules = OPPS_ADMIN_RULES.get('promos.PromoAdmin')
+    if promo_admin_rules:
+        PromoAdmin = apply_rules(PromoAdmin, promo_admin_rules)
+
+    example of settings.py
+
+    OPPS_ADMIN_RULES = {
+        'promos.PromoAdmin': {
+            'fieldsets': (
+                (u'Identification', {
+                    'fields': ('site', 'title', 'slug')}),
+            )
+        }
+    }
+    """
+
+    # apply fieldsets
+    fieldsets = rules.get('fieldsets')
+    if fieldsets:
+        new_items = [(_(item[0]), item[1]) for item in fieldsets]
+        admin_class.fieldsets = new_items
+
+    # TODO:
+    # apply list_display
+    # apply list_filter
+    # apply search_fields
+    # apply exclude
+    # apply raw_id_fields
+
+    return admin_class
