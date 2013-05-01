d791698f967d6bfee0e7506c7451f08a862baad5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-01 04:00:25 -0300

    Create class OppsView, base class OppsDetail/OppsList

--- opps/articles/views/generic.py
+++ opps/articles/views/generic.py
@@ -12,15 +12,48 @@ from opps.articles.utils import set_context_data
 from opps.channels.models import Channel
 
 
-class OppsList(ListView):
+class OppsView(object):
 
     context_object_name = "context"
     paginate_by = settings.OPPS_PAGINATE_BY
     limit = settings.OPPS_VIEWS_LIMIT
 
     def __init__(self):
+
         self.slug = None
+        self.channel = None
+        self.long_slug = None
         self.channel_long_slug = []
+        self.article = self.model.objects.all()
+
+    def get_template_folder(self):
+        domain_folder = self.type
+        if self.site.id > 1:
+            domain_folder = "{0}/{1}".format(self.site, self.type)
+        return domain_folder
+
+    def get_long_slug(self):
+        self.long_slug = self.kwargs.get('channel__long_slug', None)
+        try:
+            if not self.long_slug:
+                self.long_slug = Channel.objects.get_homepage(
+                    site=self.site).long_slug
+        except AttributeError:
+            pass
+        return self.long_slug
+
+    def set_channel_rules(self):
+        self.channel = get_object_or_404(Channel, site=self.site,
+                                         long_slug=self.long_slug,
+                                         date_available__lte=timezone.now(),
+                                         published=True)
+
+        self.channel_long_slug = [self.long_slug]
+        for children in self.channel.get_children():
+            self.channel_long_slug.append(children)
+
+
+class OppsList(OppsView, ListView):
 
     def get_context_data(self, **kwargs):
         return set_context_data(self, OppsList, **kwargs)
@@ -28,9 +61,7 @@ class OppsList(ListView):
     @property
     def template_name(self):
 
-        domain_folder = self.type
-        if self.site.id > 1:
-            domain_folder = "{0}/{1}".format(self.site, self.type)
+        domain_folder = self.get_template_folder()
 
         if self.channel:
             if self.channel.group and self.channel.parent:
@@ -47,25 +78,13 @@ class OppsList(ListView):
     @property
     def queryset(self):
         self.site = get_current_site(self.request)
+        self.long_slug = self.get_long_slug()
 
-        self.long_slug = self.kwargs.get('channel__long_slug', None)
-        try:
-            if not self.long_slug:
-                self.long_slug = Channel.objects.get_homepage(
-                    site=self.site).long_slug
-        except AttributeError:
-            self.channel = None
-            self.long_slug = None
+        if not self.long_slug:
             return None
 
-        self.channel = get_object_or_404(Channel, site=self.site,
-                                         long_slug=self.long_slug,
-                                         date_available__lte=timezone.now(),
-                                         published=True)
+        self.set_channel_rules()
 
-        self.channel_long_slug = [self.long_slug]
-        for children in self.channel.get_children():
-            self.channel_long_slug.append(children)
         self.article = self.model.objects.filter(
             site=self.site,
             channel_long_slug__in=self.channel_long_slug,
@@ -75,19 +94,14 @@ class OppsList(ListView):
         return self.article
 
 
-class OppsDetail(DetailView):
-
-    context_object_name = "context"
-    limit = settings.OPPS_VIEWS_LIMIT
+class OppsDetail(OppsView, DetailView):
 
     def get_context_data(self, **kwargs):
         return set_context_data(self, OppsDetail, **kwargs)
 
     @property
     def template_name(self):
-        domain_folder = self.type
-        if self.site.id > 1:
-            domain_folder = "{0}/{1}".format(self.site, self.type)
+        domain_folder = self.get_template_folder()
 
         try:
             _template = '{0}/{1}/{2}.html'.format(
@@ -101,25 +115,11 @@ class OppsDetail(DetailView):
     def queryset(self):
         self.site = get_current_site(self.request)
         self.slug = self.kwargs.get('slug')
-
-        self.long_slug = self.kwargs.get('channel__long_slug', None)
-        try:
-            if not self.long_slug:
-                Channel.objects.get_homepage(site=self.site).long_slug
-        except AttributeError:
-            pass
-
+        self.long_slug = self.get_long_slug()
         if not self.long_slug:
             return None
 
-        self.channel = get_object_or_404(Channel, site=self.site,
-                                         long_slug=self.long_slug,
-                                         date_available__lte=timezone.now(),
-                                         published=True)
-
-        self.channel_long_slug = [self.long_slug]
-        self.channel_long_slug.append(
-            [children.long_slug for children in self.channel.get_children()])
+        self.set_channel_rules()
 
         self.article = self.model.objects.filter(
             site=self.site,
