30b2a36fdf4a45962b9fdb7bf904b50722258707
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-03 20:12:53 -0300

    Create signals redirect_generate on articles

---
+++ opps/articles/signals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.redirects.models import Redirect
+
+
+def redirect_generate(sender, instance, created, **kwargs):
+    obj, create = Redirect.objects.get_or_create(
+        old_path=instance.get_absolute_url(),
+        site=instance.site,
+        new_path=instance.url)
+    obj.save()
