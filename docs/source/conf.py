746666e26823d170984ebaa63c3230dd098cb637
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-05 03:41:33 -0300

    upatde copyright on docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'opps'
-copyright = u'2013, YACOWS'
+copyright = u'2013, Opps Project'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
