3b4e775c5270b6eb8e14cc7fca2d3b3bb1fa0f21
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-07 14:25:27 -0300

    Remove import Count, not used

--- opps/articles/models.py
+++ opps/articles/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.db.models import Count
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
