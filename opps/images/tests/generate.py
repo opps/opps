057ab4580dea5e8a8210a899be4c37b06245ae10
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-09 19:09:02 -0300

    Add basic test on images generate url

---
+++ opps/images/tests/generate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from mock import patch
+from unittest import TestCase
+from django.template import Template, Context
+
+
+class TestThumborURLTTagMock(TestCase):
+    url = 'oppsproject.org/path/image.jpg'
+    generate_url_path = 'opps.images.templatetags.images_tags.image_url'
+
+    def render(self, arguments):
+        source = u'{% load images_tags %}{% image_url '+ arguments +' %}'
+        template = Template(source)
+        rendered = template.render(Context({'url': self.url}))
+        return rendered.strip()
+
+    def test_should_pass_the_image_url_arg_to_the_helper(self):
+        self.assertEqual(self.render(u'url unsafe=True'),
+                         u'http://localhost:8888/unsafe/localhost:8000/media/'
+                         u'oppsproject.org/path/image.jpg')
+
+    def test_should_pass_kwargs_to_the_helper(self):
+        self.assertEqual(self.render(u'url width=300 height=200 unsafe=True'),
+                         u'http://localhost:8888/unsafe/300x200/'
+                         u'localhost:8000/media/oppsproject.org/path/'
+                         u'image.jpg')
