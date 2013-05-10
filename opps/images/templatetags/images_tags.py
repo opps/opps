0c6a27b483fdfaf04c0481151d2c3e282e4eca4f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-10 13:04:49 -0300

    Create template tag image obj on images receive obj image

--- opps/images/templatetags/images_tags.py
+++ opps/images/templatetags/images_tags.py
@@ -10,3 +10,19 @@ register = template.Library()
 @register.simple_tag
 def image_url(image_url, **kwargs):
     return url(image_url=image_url, **kwargs)
+
+
+@register.simple_tag
+def image_obj(image, **kwargs):
+    new = {}
+    new['flip'] = image.flip
+    new['flop'] = image.flop
+    if image.halign:
+        new['halign'] = image.halign
+    if image.valign:
+        new['valign'] = image.valign
+    new['fit_in'] = image.fit_in
+    new['smart'] = image.smart
+
+    kwargs = dict(new, **kwargs)
+    return url(image_url=image.image.url, **kwargs)
