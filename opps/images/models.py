9be3d64a77774624ba5976ea019df7c2abd049b4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-10 01:55:20 -0300

    create class mdoel Cropping on Images app

--- opps/images/models.py
+++ opps/images/models.py
@@ -10,7 +10,19 @@ from django.utils import timezone
 from taggit.models import TaggedItemBase
 from taggit.managers import TaggableManager
 
-from opps.core.models import Publishable
+from opps.core.models import Publishable, Slugged
+
+
+HALIGN_CHOICES = (
+    ('left', _('Left')),
+    ('center', _('Center')),
+    ('right', _('Right'))
+)
+VALIGN_CHOICES = (
+    ('top', _('Top')),
+    ('middle', _('Middle')),
+    ('bottom', _('Bottom'))
+)
 
 
 def get_file_path(instance, filename):
@@ -27,11 +39,54 @@ class TaggedImage(TaggedItemBase):
     pass
 
 
-class Image(Publishable):
+class Cropping(models.Model):
+    crop_example = models.CharField(_(u"Crop Example"), max_length=140,
+                                   null=True, blank=True)
+    flip = models.BooleanField(_(u'Flip'), default=False,
+                               help_text=_(u'Flag that indicates that '
+                                           u'thumbor should flip '
+                                           u'horizontally (on the vertical '
+                                           u'axis) the image'))
+    flop = models.BooleanField(_(u'Flop'), default=False,
+                               help_text=_(u'Flag that indicates that '
+                                           u'thumbor should flip '
+                                           u'vertically (on the horizontal '
+                                           u'axis) the image'))
+    halign = models.CharField(_(u'Horizontal Align'), default=False,
+                              max_length=6,
+                              null=True, blank=True,
+                              choices=HALIGN_CHOICES,
+                              help_text=_(u'Horizontal alignment that '
+                                          u'thumbor should use for cropping'))
+    valign = models.CharField(_(u'Vertical Align'), default=False,
+                              max_length=6,
+                              null=True, blank=True,
+                              choices=VALIGN_CHOICES,
+                              help_text=_(u'Vertical alignment that '
+                                          u'thumbor should use for cropping'))
+
+    fit_in = models.BooleanField(_(u'Fit in'), default=False,
+                               help_text=_(u'flag that indicates that '
+                                           u'thumbor should fit the image in '
+                                           u'the box defined by width x '
+                                           u'height'))
+
+    smart = models.BooleanField(_(u'Smart'), default=False,
+                                help_text=_(u'Flag that indicates that'
+                                            u'thumbor should use smart '
+                                            u'cropping;'))
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        self.crop_example = self.image.url
+        super(Cropping, self).save(*args, **kwargs)
+
+
+class Image(Publishable, Slugged, Cropping):
 
     title = models.CharField(_(u"Title"), max_length=140, db_index=True)
-    slug = models.SlugField(_(u"Slug"), max_length=150, blank=True,
-                            db_index=True)
     image = models.ImageField(upload_to=get_file_path)
     description = models.TextField(_(u"Description"), null=True, blank=True)
     tags = TaggableManager(blank=True, through=TaggedImage, verbose_name=u'Tags')
@@ -41,9 +96,10 @@ class Image(Publishable):
     class META:
         verbose_name = _('Image')
         verbose_name_plural = _('Images')
+        unique_together = ['site', 'slug']
 
     def __unicode__(self):
-        return self.slug
+        return u"{}-{}".format(self.site, self.slug)
 
     def get_absolute_url(self):
         if self.date_available <= timezone.now() and self.published:
