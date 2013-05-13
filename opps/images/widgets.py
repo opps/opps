ea046e8996c3bbad95578ef3209b62972d88e720
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-13 20:24:11 -0300

    Add _height/_width on widget MultipleUpload

--- opps/images/widgets.py
+++ opps/images/widgets.py
@@ -6,10 +6,16 @@ from django.template.loader import render_to_string
 class MultipleUpload(forms.FileInput):
     def render(self, name, value, attrs=None):
         _value = ""
+        _height = ""
+        _width = ""
         if value:
             _value = "{0}{1}".format(settings.MEDIA_URL, value)
+            _height = value.height
+            _width = value.width
         return render_to_string("admin/opps/images/multiupload.html",
                                 {"name": name, "value": _value,
+                                 'height': _height,
+                                 'width': _width,
                                  "STATIC_URL": settings.STATIC_URL})
 
 
