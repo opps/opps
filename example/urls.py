d37fc90052aa051b0c5b2290dae0c65d4fc785c4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-13 01:47:26 -0300

    Add static/media url

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 
 urlpatterns = patterns(
     '',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
     url(r'^', include('opps.urls')),
 )
