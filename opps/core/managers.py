dbe9d5d1c4b2655e0bca4f099755caae94dcfe89
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-17 04:51:07 -0300

    Custom get method manager, check cache or process .get()

--- opps/core/managers.py
+++ opps/core/managers.py
@@ -18,3 +18,21 @@ class CacheManager(models.Manager):
                                   self.model._meta.db_table,
                                   id)
 
+    def get(self, *args, **kwargs):
+        id = repr(kwargs)
+        pointer_key = self.__cache_key(id)
+        model_key = cache.get(pointer_key)
+
+        if model_key is not None:
+            model = cache.get(model_key)
+            if model is not None:
+                return model
+
+        model = super(CacheManager, self).get(*args, **kwargs)
+
+        if not model_key:
+            model_key = self.__cache_key(model, model.pk)
+            cache.set(pointer_key, model_key, settings.CACHE_EXPIRE)
+
+        cache.set(model_key, model, settings.CACHE_EXPIRE)
+        return model
