4a4b316681aff4991e2f1c0c5f76f9ce28e26281
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-25 01:37:35 -0300

    Test 404 error

--- opps/articles/views/generic.py
+++ opps/articles/views/generic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.contrib.sites.models import get_current_site
@@ -55,6 +55,31 @@ class OppsView(object):
                 article__slug=self.slug)
 
         return context
+
+    def get_object(self, queryset=None):
+        if queryset is None:
+            queryset = self.get_queryset()
+        pk = self.kwargs.get(self.pk_url_kwarg, None)
+        slug = self.kwargs.get(self.slug_url_kwarg, None)
+
+        if pk is not None:
+            queryset = queryset.filter(pk=pk)
+
+        elif slug is not None:
+            slug_field = self.get_slug_field()
+            queryset = queryset.filter(**{slug_field: slug})
+        else:
+            raise AttributeError("Generic detail view %s must be called with "
+                                 "either an object"
+                                 "pk or a slug." % self.__class__.__name__)
+
+        try:
+            obj = queryset.get()
+        except ObjectDoesNotExist:
+            pass
+
+        return obj
+
 
     def get_template_folder(self):
         domain_folder = self.type
