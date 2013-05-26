f2d1f631c3397d86034bff4e7c36306f29f47bff
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-26 16:30:41 -0300

    DeprecationWarning: the md5 module is deprecated; use hashlib instead

--- opps/core/cache.py
+++ opps/core/cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf import settings
-import md5
+from hashlib import md5
 
 
 def _cache_key(_type, model, site, channel_long_slug):
-    return md5.md5((u'{}:{}:{}:{}:{}'.format(
+    return md5((u'{}:{}:{}:{}:{}'.format(
         _type,
         settings.OPPS_CACHE_PREFIX,
         model._meta.db_table,
