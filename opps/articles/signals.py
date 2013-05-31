4e15a7cd9162dd3be44ffae85387caf4d12990f3
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-05-31 12:43:42 -0300

    Remove signals articles

--- opps/articles/signals.py
+++ opps/articles/signals.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.contrib.redirects.models import Redirect
-from googl.short import GooglUrlShort
 
 
 def redirect_generate(sender, instance, created, **kwargs):
@@ -10,28 +9,3 @@ def redirect_generate(sender, instance, created, **kwargs):
         site=instance.site,
         new_path=instance.url)
     obj.save()
-
-
-def shorturl_generate(sender, instance, created, **kwargs):
-    if not instance.short_url:
-        instance.short_url = GooglUrlShort(
-            instance.get_http_absolute_url()
-        ).short()
-        instance.save()
-
-
-def delete_article(sender, instance, using, **kwargs):
-    """
-    Ensure objects are deleted from mother class Article
-    when deleted from child class
-    """
-    try:
-        instance.__class__.objects.filter(
-            child_class=instance.child_class,
-            slug=instance.slug,
-            channel=instance.channel,
-            site=instance.site
-        ).delete()
-    except instance.__class__.DoesNotExist:
-        # object not exists
-        pass
