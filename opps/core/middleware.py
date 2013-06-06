d8a56d3d9950856cb9a29a215ad139c0309dac3d
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-06 12:15:31 -0300

    Remove DynamicSiteMiddleware core app

--- opps/core/middleware.py
+++ opps/core/middleware.py
@@ -34,35 +34,6 @@ class TemplateContextMiddleware(object):
         return response
 
 
-class DynamicSiteMiddleware(object):
-
-    def hosting_parse(self, hosting):
-        """
-        Returns ``(host, port)`` for ``hosting`` of the form ``'host:port'``.
-
-        If hosting does not have a port number, ``port`` will be None.
-        """
-        if ':' in hosting:
-            return hosting.rsplit(':', 1)
-        return hosting, None
-
-    def get_hosting(self, hosting):
-        domain, port = self.hosting_parse(hosting)
-        if domain in settings.OPPS_DEFAULT_URLS:
-            domain = 'example.com'
-        try:
-            return Site.objects.get(domain=domain)
-        except Site.DoesNotExist:
-            return Site.objects.all()[0]
-
-    def process_request(self, request):
-        hosting = request.get_host().lower()
-        site = self.get_hosting(hosting)
-
-        settings.SITE_ID = site.id
-        settings.CACHE_MIDDLEWARE_KEY_PREFIX = "opps_site-{}".format(site.id)
-
-
 class MobileDetectionMiddleware(object):
     u"""Used django-mobile core
 
