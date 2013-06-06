987f641deaccd14d7fb3d3ef9986796fd62e8b14
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-06 12:16:41 -0300

    Add DynamicSiteMiddleware on opps contrib multisite

---
+++ opps/contrib/multisite/middleware.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+
+class DynamicSiteMiddleware(object):
+
+    def hosting_parse(self, hosting):
+        """
+        Returns ``(host, port)`` for ``hosting`` of the form ``'host:port'``.
+
+        If hosting does not have a port number, ``port`` will be None.
+        """
+        if ':' in hosting:
+            return hosting.rsplit(':', 1)
+        return hosting, None
+
+    def get_hosting(self, hosting):
+        domain, port = self.hosting_parse(hosting)
+        if domain in settings.OPPS_DEFAULT_URLS:
+            domain = 'example.com'
+        try:
+            return Site.objects.get(domain=domain)
+        except Site.DoesNotExist:
+            return Site.objects.all()[0]
+
+    def process_request(self, request):
+        hosting = request.get_host().lower()
+        site = self.get_hosting(hosting)
+
+        settings.SITE_ID = site.id
+        settings.CACHE_MIDDLEWARE_KEY_PREFIX = "opps_site-{}".format(site.id)
