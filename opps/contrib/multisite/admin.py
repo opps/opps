146e35f48774173c2000b8a9790cdbe6925ba94a
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-10 20:34:14 -0300

    Create AdminViewPermission on contrib multisite

--- opps/contrib/multisite/admin.py
+++ opps/contrib/multisite/admin.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils import timezone
+
 from .models import SitePermission
+
+
+class AdminViewPermission(admin.ModelAdmin):
+
+    def queryset(self, request):
+        queryset = super(AdminViewPermission, self).queryset(request)
+        try:
+            sitepermission = SitePermission.objects.get(
+                user=request.user,
+                date_available__lte=timezone.now(),
+                published=True)
+            return queryset.filter(site_iid=sitepermission.site_iid)
+        except SitePermission.DoesNotExist:
+            pass
+        return queryset
 
 
 admin.site.register(SitePermission)
