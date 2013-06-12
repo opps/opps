6b48aa6fdc526aa760834b9b8fbfc50f2e1cf0e4
Bruno Rocha <rochacbruno@gmail.com>
2013-06-12 02:18:36 -0300

    Cookie based mobile template redirection

--- opps/core/middleware.py
+++ opps/core/middleware.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 import re
+from types import MethodType
+
 from django.contrib.sites.models import Site
 from django.contrib.sites.models import get_current_site
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.http import SimpleCookie, HttpRequest
 
 from opps.channels.models import Channel
 
@@ -138,10 +141,68 @@ class MobileDetectionMiddleware(object):
                     settings.OPPS_DOMAIN_MOBILE))
 
 
-class MobileRedirectMiddleware(object):
+def _set_cookie(self, key, value='', max_age=None, expires=None, path='/',
+           domain=None, secure=False):
+    self._resp_cookies[key] = value
+    self.COOKIES[key] = value
+    if max_age is not None:
+        self._resp_cookies[key]['max-age'] = max_age
+    if expires is not None:
+        self._resp_cookies[key]['expires'] = expires
+    if path is not None:
+        self._resp_cookies[key]['path'] = path
+    if domain is not None:
+        self._resp_cookies[key]['domain'] = domain
+    if secure:
+        self._resp_cookies[key]['secure'] = True
 
+
+def _delete_cookie(self, key, path='/', domain=None):
+    self.set_cookie(key, max_age=0, path=path, domain=domain,
+                    expires='Thu, 01-Jan-1970 00:00:00 GMT')
+    try:
+        del self.COOKIES[key]
+    except KeyError:
+        pass
+
+
+class MobileRedirectMiddleware(object):
+    """
+    Allows setting and deleting of cookies from requests in exactly the same
+    way as responses.
+
+    >>> request.set_cookie('name', 'value')
+
+    The set_cookie and delete_cookie are exactly the same as the ones built
+    into the Django HttpResponse class.
+
+    http://docs.djangoproject.com/en/dev/ref/request-response
+        /#django.http.HttpResponse.set_cookie
+    """
     def process_request(self, request):
-        domain = request.META.get('HTTP_HOST', '')
-        mobile_domain = settings.OPPS_DOMAIN_MOBILE
-        if domain == mobile_domain:
+
+        current_cookie = request.COOKIES.get('template_mode', None)
+        template_mode = request.GET.get('template_mode', None)
+        settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_WEB
+
+        if not template_mode and not current_cookie:
+            return
+
+        request._resp_cookies = SimpleCookie()
+        request.set_cookie = MethodType(_set_cookie, request, HttpRequest)
+        request.delete_cookie = MethodType(
+            _delete_cookie, request, HttpRequest
+        )
+
+        if template_mode:
+            request.set_cookie('template_mode', template_mode)
+            current_cookie = template_mode
+
+        if current_cookie and current_cookie.strip().lower() == "mobile":
             settings.TEMPLATE_DIRS = settings.TEMPLATE_DIRS_MOBILE
+
+    def process_response(self, request, response):
+        if hasattr(request, '_resp_cookies') and request._resp_cookies:
+            response.cookies.update(request._resp_cookies)
+
+        return response
