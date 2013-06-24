49836fd204886450abbfb4dba628fa389247bd54
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-06-24 12:49:08 -0300

    Used new haystack format on articles

--- opps/articles/search_indexes.py
+++ opps/articles/search_indexes.py
@@ -2,22 +2,15 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from django.conf import settings
-
-from haystack.indexes import SearchIndex, CharField, DateTimeField
-from haystack import site
+from haystack.indexes import SearchIndex, Indexable, CharField, DateTimeField
 
 from .models import Post, Album
 
-migration_date = getattr(settings, 'MIGRATION_DATE', None)
-if migration_date:
-    m_date = datetime.strptime(migration_date, "%Y-%m-%d").date()
-    Post.is_legacy = lambda self: m_date >= self.date_insert.date()
-else:
-    Post.is_legacy = lambda self: False
+
+Post.is_legacy = lambda self: False
 
 
-class PostIndex(SearchIndex):
+class PostIndex(SearchIndex, Indexable):
     text = CharField(document=True, use_template=True)
     date_available = DateTimeField(model_attr='date_available')
     date_update = DateTimeField(model_attr='date_update')
@@ -25,13 +18,16 @@ class PostIndex(SearchIndex):
     def get_updated_field(self):
         return 'date_update'
 
-    def index_queryset(self):
-        return Post.objects.filter(
+    def get_model(self):
+        return Post
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.filter(
             date_available__lte=datetime.now(),
             published=True)
 
 
-class AlbumIndex(SearchIndex):
+class AlbumIndex(SearchIndex, Indexable):
     text = CharField(document=True, use_template=True)
     date_available = DateTimeField(model_attr='date_available')
     date_update = DateTimeField(model_attr='date_update')
@@ -39,10 +35,10 @@ class AlbumIndex(SearchIndex):
     def get_updated_field(self):
         return 'date_update'
 
-    def index_queryset(self):
-        return Album.objects.filter(
+    def get_model(self):
+        return Album
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.filter(
             date_available__lte=datetime.now(),
             published=True)
-
-site.register(Post, PostIndex)
-site.register(Album, AlbumIndex)
