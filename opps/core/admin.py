40985493b8185f94fd841b960f39d4a668fce132
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-02 11:10:59 -0300

    Use archive on image_thumb (admin)

--- opps/core/admin.py
+++ opps/core/admin.py
@@ -123,7 +123,7 @@ class PublishableAdmin(admin.ModelAdmin):
     def image_thumb(self, obj):
         if obj.main_image:
             return u'<img width="60px" height="60px" src="{0}" />'.format(
-                image_url(obj.main_image.image.url, width=60, height=60))
+                image_url(obj.main_image.archive.url, width=60, height=60))
         return _(u'No Image')
     image_thumb.short_description = _(u'Thumbnail')
     image_thumb.allow_tags = True
