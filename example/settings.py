2dd972c95e48da1080fd357f425902b34937bcc5
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-05 13:01:05 -0300

    Set new name for sqlite example

--- example/settings.py
+++ example/settings.py
@@ -10,7 +10,7 @@ PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_PATH, 'dev.db'),
+        'NAME': os.path.join(PROJECT_PATH, '../example.db'),
     }
 }
 
