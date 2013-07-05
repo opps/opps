925e6a0dc102c3ebb296412a9e3d8df0144236a6
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-05 01:01:14 -0300

    remove the word 'dev' version.

--- opps/__init__.py
+++ opps/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 1, 9, 'dev')
+VERSION = (0, 1, 9)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
