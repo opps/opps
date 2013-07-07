f19992b2f25e299b41cf99d6267bd1f0e28757d1
Bruno Rocha <rochacbruno@gmail.com>
2013-07-07 19:06:39 -0300

    Fix image test

--- opps/images/tests/generate.py
+++ opps/images/tests/generate.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 from django.template import Template, Context
+from django.conf import settings
 
 
 class TestImagesTagsImageUrl(TestCase):
@@ -18,12 +19,21 @@ class TestImagesTagsImageUrl(TestCase):
         self.assertTrue(self.render(u'url unsafe=True'))
 
     def test_should_pass_the_image_url_arg_to_the_helper(self):
-        self.assertEqual(self.render(u'url unsafe=True'),
-                         u'http://localhost:8888/unsafe/localhost:8000/media/'
+        if settings.THUMBOR_ENABLED:
+            image_url = (u'http://localhost:8888/unsafe/localhost:8000/media/'
                          u'oppsproject.org/path/image.jpg')
+        else:
+            image_url = self.url
+
+        self.assertEqual(self.render(u'url unsafe=True'), image_url)
 
     def test_should_pass_kwargs_to_the_helper(self):
-        self.assertEqual(self.render(u'url width=300 height=200 unsafe=True'),
-                         u'http://localhost:8888/unsafe/300x200/'
+        if settings.THUMBOR_ENABLED:
+            image_url = (u'http://localhost:8888/unsafe/300x200/'
                          u'localhost:8000/media/oppsproject.org/path/'
                          u'image.jpg')
+        else:
+            image_url = self.url
+
+        self.assertEqual(self.render(u'url width=300 height=200 unsafe=True'),
+                         image_url)
