aabacf165fc0c975285923d933409a4a5d466c88
Bruno Rocha <rochacbruno@gmail.com>
2013-07-07 21:15:40 -0300

    get_config can read the dict in settings

--- opps/core/templatetags/config.py
+++ opps/core/templatetags/config.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from django import template
+from django.conf import settings
 from opps.core.utils import get_app_model
 
 register = template.Library()
+
+CONFIG_DICT = getattr(settings, 'OPPS_GET_CONFIG_DICT', None)
 
 
 @register.simple_tag
@@ -13,10 +16,19 @@ def get_config(appname, key, **kwargs):
     {% get_config 'polls' 'key_slug' %}
     {% get_config 'infographics' 'key_slug' %}
     {% get_config 'promos' 'key_slug' %}
+    {% get_config 'channels' 'color' channel__long_slug='/home' %}
 
     Also works
     {% get_config 'opps.polls' 'key_slug' %}
     """
+    if CONFIG_DICT:
+        try:
+            channel = kwargs['channel__long_slug']
+            value = CONFIG_DICT.get(channel, {}).get(key, None)
+            if value:
+                return value
+        except:
+            pass
 
     model = get_app_model(appname, "Config")
     return model and model.get_value(key, **kwargs)
