aebb5ebacb49ad56b633411214ad3384b44f7bd4
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-10 11:53:14 -0300

    Fix middleware Opps 0.2

--- example/settings.py
+++ example/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
     # Used in Multi-Site
-    'opps.core.middleware.DynamicSiteMiddleware',
+    'opps.contrib.multisite.middleware.DynamicSiteMiddleware',
     # Used in Mobile
-    'opps.core.middleware.MobileDetectionMiddleware',
+    'opps.contrib.mobile.middleware.MobileDetectionMiddleware',
 )
 
 TEMPLATE_DIRS = (os.path.join(PROJECT_PATH, 'src', 'templates'),)
