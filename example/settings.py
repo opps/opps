8f9da2bc3fe011fed80ba5337e0553f7528d0272
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-16 19:19:22 -0300

    Used django admin default

--- example/settings.py
+++ example/settings.py
@@ -66,8 +66,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.redirects',
+    'django.contrib.admin',
 
-    'opps.contrib.admin',
     'opps.containers',
     'opps.core',
     'opps.archives',
@@ -81,8 +81,6 @@ INSTALLED_APPS = (
 
     'south',
     'taggit',
-    'reversion',
-    'crispy_forms',
 )
 
 OPPS_CHECK_MOBILE = True
