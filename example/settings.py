8935681adecb9a2ef6938073ff9ed15d5b613a7e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-18 02:58:20 -0300

    Add formtools on settings

--- example/settings.py
+++ example/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.redirects',
+    'django.contrib.formtools',
     'opps.contrib.admin',
     'django.contrib.admin',
 
