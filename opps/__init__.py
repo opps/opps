a07228aac8f4af8c04f7f844b48fd75ec4f2b25f
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-19 16:34:18 -0300

    Set new developer version

--- opps/__init__.py
+++ opps/__init__.py
@@ -4,7 +4,7 @@ import pkg_resources
 
 pkg_resources.declare_namespace(__name__)
 
-VERSION = (0, 1, 10)
+VERSION = (0, 1, 11)
 
 __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
