b5c1451300e07125a963e8a3c96a77a8f387f78b
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-19 22:11:16 -0300

    create all api views

---
+++ opps/api/views/generic/detail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.sites.models import get_current_site
+from django.utils import timezone
+
+from rest_framework.generics import ListAPIView as RestListAPIView
+
+from opps.views.generic.base import View
+
+
+class DetailView(View, RestListAPIView):
+    def get_queryset(self):
+        self.site = get_current_site(self.request)
+        self.slug = self.kwargs.get('slug')
+        self.long_slug = self.get_long_slug()
+        if not self.long_slug:
+            return None
+
+        self.set_channel_rules()
+
+        filters = {}
+        filters['site_domain'] = self.site.domain
+        filters['channel_long_slug'] = self.long_slug
+        filters['slug'] = self.slug
+
+        preview_enabled = self.request.user and self.request.user.is_staff
+
+        if not preview_enabled:
+            filters['date_available__lte'] = timezone.now()
+            filters['published'] = True
+
+        queryset = super(DetailView, self).get_queryset()
+        return queryset.filter(**filters)._clone()
---
+++ opps/api/views/generic/list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.sites.models import get_current_site
+from django.utils import timezone
+
+from rest_framework.generics import ListAPIView as RestListAPIView
+
+from opps.views.generic.base import View
+from opps.containers.models import ContainerBox
+
+
+class ListView(View, RestListAPIView):
+    def get_queryset(self):
+        self.long_slug = self.get_long_slug()
+        self.site = get_current_site(self.request)
+
+        if not self.long_slug:
+            return None
+
+        self.set_channel_rules()
+
+        self.articleboxes = ContainerBox.objects.filter(
+            channel__long_slug=self.long_slug)
+
+        for box in self.articleboxes:
+            self.excluded_ids.update([a.pk for a in box.ordered_containers()])
+
+        queryset = super(ListView, self).get_queryset()
+        filters = {}
+        filters['site_domain'] = self.site.domain
+        filters['channel_long_slug__in'] = self.channel_long_slug
+        filters['date_available__lte'] = timezone.now()
+        filters['published'] = True
+        filters['show_on_root_channel'] = True
+        queryset = queryset.filter(**filters).exclude(pk__in=self.excluded_ids)
+
+        return queryset._clone()
