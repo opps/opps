63aa58e08203676f5b83970445ad80241ad50b8d
Ellison Leao <ellisonleao@gmail.com>
2013-07-29 12:33:25 -0300

    Add simple TagList view for tags

--- opps/core/tags/views.py
+++ opps/core/tags/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+# -*- encoding: utf-8 -*-
+from django.utils import timezone
+from django.contrib.sites.models import get_current_site
+
+from opps.views.generic.list import ListView
+from opps.containers.models import Container
+
+
+class TagList(ListView):
+    model = Container
+    template_name_suffix = '_tags'
+
+    def get_queryset(self):
+        self.site = get_current_site(self.request)
+        self.long_slug = self.kwargs['tag']
+        self.containers = self.model.objects.filter(
+            site_domain=self.site,
+            tags__in=self.long_slug,
+            date_available__lte=timezone.now(),
+            published=True).all()
+        return self.containers
