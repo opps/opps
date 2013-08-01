d6ac23ac0cc8e92fcd736cee39beb93409a8cc9f
Bruno Rocha <rochacbruno@gmail.com>
2013-07-31 22:22:52 -0300

    Added DEFAULT TinyMCE configuration dict

--- opps/core/__init__.py
+++ opps/core/__init__.py
@@ -30,7 +30,31 @@ class OppsCoreConf(AppConf):
     LIST_MODELS = ('Post',)
     RECOMMENDATION_RANGE_DAYS = 180
     SMART_SLUG_ENABLED = True
-    OPPS_EDITOR = {}
+
+    # default settings for tinymce
+    OPPS_EDITOR = {
+        'editor': 'tinymce',
+        'js': ('/static/tinymce/tinymce.min.js',),
+        "theme": "modern",
+        "plugins": [
+            """advlist autolink lists link image charmap print preview hr
+        anchor pagebreak """,
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            """insertdatetime media nonbreaking save table contextmenu
+        directionality""",
+            "emoticons template paste textcolor"
+        ],
+        "toolbar1": """insertfile undo redo | styleselect | bold italic |
+                alignleft aligncenter alignright alignjustify |
+                bullist numlist outdent indent | link image""",
+        "toolbar2": "print preview media | forecolor backcolor emoticons",
+        "image_advtab": True,
+        "templates": [
+            {"title": 'Test template 1', "content": 'Test 1'},
+            {"title": 'Test template 2', "content": 'Test 2'}
+        ],
+        "file_browser_callback": 'CustomFileBrowser',
+    }
 
     class Meta:
         prefix = 'opps'
