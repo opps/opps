6e0fd87a8796625ffe9975786d6b5469e1d27876
Ellison Leao <ellisonleao@gmail.com>
2013-08-01 15:24:51 -0300

    Add requirements_dev and runtests file

---
+++ runtests.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import sys
+
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        MIDDLEWARE_CLASSES = (
+            'django.middleware.common.CommonMiddleware',
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.messages.middleware.MessageMiddleware',
+            'django.contrib.redirects.middleware.RedirectFallbackMiddleware'
+        ),
+        INSTALLED_APPS = (
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'django.contrib.sites',
+            'django.contrib.admin',
+            'django.contrib.redirects',
+
+            'opps.core',
+            'opps.boxes',
+            'opps.channels',
+            'opps.sources',
+            'opps.articles',
+            'opps.images',
+            'opps.sitemaps',
+            'opps.flatpages',
+            'taggit',
+        ),
+        SITE_ID = 1,
+        ROOT_URLCONF = "opps.urls",
+        TEST_RUNNER = 'django_coverage.coverage_runner.CoverageRunner',
+        HAYSTACK_CONNECTIONS = {
+            'default': {
+                'ENGINE': 'haystack.backends.simple_backend.SimpleEngine'
+            }
+        }
+
+    )
+
+
+def runtests():
+    argv = sys.argv[:1] + ['test'] + ['core', 'articles', 'boxes',
+                                      'channels', 'images', 'sources',
+                                      'sitemaps', 'flatpages']
+    execute_from_command_line(argv)
+
+
+if __name__ == '__main__':
+    runtests()
+
