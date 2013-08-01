c3eca9e5bc53cc2b56dc642979622807b027186b
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-01 19:27:10 -0300

    Fix pep8, opps/db/__init__.py:5:1: E302 expected 2 blank lines, found 1

--- opps/db/__init__.py
+++ opps/db/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from .conf import settings
 
+
 def Db(*args, **kwarg):
     _import = settings.OPPS_DB_ENGINE.split('.')[-1]
     _from = u".".join(settings.OPPS_DB_ENGINE.split('.')[:-1])
