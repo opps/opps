960e7b064f6cb7aaf866161f4d4ace4cf8519b6e
Bruno Rocha <rochacbruno@gmail.com>
2013-08-03 01:06:44 -0300

    Added opps plugin to tinymce

--- opps/core/__init__.py
+++ opps/core/__init__.py
@@ -43,12 +43,12 @@ class OppsCoreConf(AppConf):
             "searchreplace wordcount visualblocks visualchars code fullscreen",
             """insertdatetime media nonbreaking save table contextmenu
         directionality""",
-            "template paste textcolor"
+            "template paste textcolor opps"
         ],
         "toolbar1": """insertfile undo redo | styleselect | bold italic |
                 alignleft aligncenter alignright alignjustify |
                 bullist numlist outdent indent | link image media |
-                print preview  | forecolor backcolor""",
+                print preview  | forecolor backcolor | opps""",
         "image_advtab": True,
         "templates": [
             {"title": 'Related', "content": RELATED_POSTS_PLACEHOLDER},
