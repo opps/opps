70eb3b055cada882ff8972ec83bdb0625e182431
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-06 14:37:09 -0300

    Add template tag callMethod

--- opps/core/templatetags/obj_tags.py
+++ opps/core/templatetags/obj_tags.py
@@ -11,3 +11,9 @@ def ofKey(value, arg):
     if value:
         return value.get(arg)
     return ""
+
+
+@register.filter
+def callMethod(obj, methodName, *args):
+    method = getattr(obj, methodName)
+    return method(*args)
