9632cca6c78f0df90544aaf52f935e0dbf26df69
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-08 16:01:56 -0300

    update tx scripts

--- scripts/tx.sh
+++ scripts/tx.sh
@@ -3,6 +3,6 @@ DIR=$(pwd)
 for i in $(ls opps/)
 do
 	echo "make $i";
-	tx set --auto-local -r opps-core.$i "opps/$i/locale/<lang>/LC_MESSAGES/django.po" --source-language=en_US --source-file "opps/$i/locale/en_US/LC_MESSAGES/django.po" --execute;\
+	tx set --auto-local -r opps-core.$i "opps/$i/locale/<lang>/LC_MESSAGES/django.po" --source-language=en --source-file "opps/$i/locale/en/LC_MESSAGES/django.po" --execute;\
 done
 
