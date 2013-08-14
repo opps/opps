eb8864e169b0db4e72fd0a0ce60678c0eedcfe18
Ellison Leao <ellisonleao@gmail.com>
2013-08-14 16:16:55 -0300

    Migrating search indexes to Haystack 2.x

--- opps/articles/search_indexes.py
+++ opps/articles/search_indexes.py
@@ -4,8 +4,7 @@ from datetime import datetime
 
 from django.conf import settings
 
-from haystack.indexes import SearchIndex, CharField, DateTimeField
-from haystack import site
+from haystack.indexes import SearchIndex, CharField, DateTimeField, Indexable
 
 from .models import Post
 
@@ -17,18 +16,18 @@ else:
     Post.is_legacy = lambda self: False
 
 
-class PostIndex(SearchIndex):
+class PostIndex(SearchIndex, Indexable):
     text = CharField(document=True, use_template=True)
     date_available = DateTimeField(model_attr='date_available')
     date_update = DateTimeField(model_attr='date_update')
 
+    def get_model(self):
+        return Post
+
     def get_updated_field(self):
         return 'date_update'
 
-    def index_queryset(self):
+    def index_queryset(self, using=None):
         return Post.objects.filter(
             date_available__lte=datetime.now(),
             published=True)
-
-
-site.register(Post, PostIndex)
