21a753fda2b5bfa9195620c2bc277d20d2be494d
Davi Oliveira <arlocm@gmail.com>
2013-08-16 23:23:45 -0300

    adding decorator opps.core.cache.cache_page

--- opps/core/cache.py
+++ opps/core/cache.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.contrib.sites.models import get_current_site
+from django.views.decorators.cache import cache_page as django_cache_page
+
 from hashlib import md5
 
 
@@ -11,3 +14,28 @@ def _cache_key(_type, model, site, channel_long_slug):
         model._meta.db_table,
         site,
         channel_long_slug)).replace(' ', '')).hexdigest()
+
+
+def cache_page(*dec_args, **dec_kwargs):
+    cache_timeout = dec_args[0] if dec_args else settings.OPPS_CACHE_EXPIRE
+    cache_alias = dec_kwargs.pop('cache', None)
+    key_prefix = dec_kwargs.pop('key_prefix', '')
+
+    def decorator(func):
+        def wrapped(*func_args, **func_kwargs):
+            request = func_args[0]
+            cache_prefix = u'{}-{}-{}'.format(
+                key_prefix,
+                get_current_site(request).domain,
+                getattr(request, 'is_mobile', False)
+            )
+
+            do_cache = django_cache_page(
+                cache_timeout,
+                cache=cache_alias,
+                key_prefix=cache_prefix
+            )(lambda *args, **kwargs: func(*func_args, **func_kwargs))
+
+            return do_cache(*func_args, **func_kwargs)
+        return wrapped
+    return decorator
