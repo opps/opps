d7e42b7ea7951fc3ae0c0b8a25bfc38bd1c49378
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-08-17 01:40:47 -0300

    add test cache page is mobile

--- opps/core/tests/test_cache.py
+++ opps/core/tests/test_cache.py
@@ -20,3 +20,19 @@ class DecoratorsTest(TestCase):
         my_view_cached2 = cache_page(123, key_prefix="test")(my_view)
         self.assertEqual(my_view_cached2(HttpRequest()), "response")
 
+    def test_cache_page_is_mobile(self):
+        """
+        Test that we can call cache_page in mobile page
+        """
+        def is_mobile_view(request):
+            return request.is_mobile
+
+        def mobile_view(request):
+            return 'response'
+
+        mobile_view_cached = cache_page(123)(mobile_view)
+        is_mobile_view_cached = cache_page(123)(is_mobile_view)
+        request = HttpRequest()
+        request.is_mobile = True
+        self.assertEqual(mobile_view_cached(request), "response")
+        self.assertTrue(is_mobile_view_cached(request))
