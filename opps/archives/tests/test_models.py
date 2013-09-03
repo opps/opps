d8abed4c4fbe859a884b9921fdf66bb75f58b7c2
Londerson <londerson@gmail.com>
2013-09-03 18:07:22 -0300

    test for archives

--- opps/archives/tests/test_models.py
+++ opps/archives/tests/test_models.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 from django.test import TestCase
 from django.db import models
+from django.contrib.sites.models import Site
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.core.files import File as DjangoFile
 
-from ..models import Archive
+from ..models import Archive, File, get_file_path
+
 
 
 class ArchiveFields(TestCase):
@@ -30,3 +37,36 @@ class ArchiveFields(TestCase):
         self.assertEqual(field.__class__, models.ForeignKey)
         self.assertTrue(field.null)
         self.assertTrue(field.blank)
+
+
+class ArchiveTest(TestCase):
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create(username=u'test', password='test')
+        self.site = Site.objects.filter(name=u'example.com').get()
+        self.file = File.objects.create(user=self.user, site=self.site,
+                                             archive=DjangoFile(open("README.rst"),
+                                             "test.png"),
+                                             title=u"Test")
+
+    def test_get_file_path(self):
+        path = get_file_path(self.file, "test.png")
+        d = datetime.now()
+        self.assertTrue(path)
+        self.assertIn(u"archives/{}".format(d.strftime("%Y/%m/%d/")), path)
+        self.assertIn(self.file.slug[:100], path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
