66889b8633fd126b3a7927a197bc9fff9660e38e
Hersonls <hersonls@gmail.com>
2013-09-03 11:49:56 -0300

    Fixed tags list view and added tag var to context

--- opps/core/tags/views.py
+++ opps/core/tags/views.py
@@ -10,12 +10,17 @@ class TagList(ListView):
     model = Container
     template_name_suffix = '_tags'
 
+    def get_context_data(self, **kwargs):
+        context = super(TagList, self).get_context_data(**kwargs)
+        context['tag'] = self.kwargs['tag']
+        return context
+
     def get_queryset(self):
         self.site = get_current_site(self.request)
         self.long_slug = self.kwargs['tag']
         self.containers = self.model.objects.filter(
             site_domain=self.site,
-            tags__in=self.long_slug,
+            tags__contains=self.long_slug,
             date_available__lte=timezone.now(),
             published=True).all()
         return self.containers
