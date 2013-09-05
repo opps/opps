d502956a3dafdf82e6c2cabd624d95e885925b5c
Ellison Leao <ellisonleao@gmail.com>
2013-09-05 20:27:11 -0300

    Fix typo

--- opps/channels/tests/test_models.py
+++ opps/channels/tests/test_models.py
@@ -180,7 +180,7 @@ class ChannelModelTest(TestCase):
         self.assertTrue(channel)
         self.assertEqual(channel.slug, self.parent.slug)
 
-    def test_not_set_homeoage(self):
+    def test_not_set_homepage(self):
         """
         not set channel home page, return none
         """
