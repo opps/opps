d058a455e93e11e071efb6e066d45b684ea932fe
Ellison Leao <ellisonleao@gmail.com>
2013-09-05 13:06:42 -0300

    Add FileAdmin

--- opps/archives/admin.py
+++ opps/archives/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
-# from opps.core.admin import PublishableAdmin
-# from opps.core.admin import apply_opps_rules
+from opps.core.admin import apply_opps_rules
+from opps.contrib.multisite.admin import AdminViewPermission
 
 from .models import File
 
 
-admin.site.register(File)
+@apply_opps_rules('archives')
+class FileAdmin(AdminViewPermission):
+    search_fields = ['title', 'slug']
+    raw_id_fields = ['user', 'source']
+    ordering = ('-date_available',)
+    list_filter = ['date_available', 'published']
+    prepopulated_fields = {"slug": ["title"]}
+
+    fieldsets = (
+        (_(u'Identification'), {
+            'fields': ('site', 'title', 'slug',)}),
+        (_(u'Content'), {
+            'fields': ('description', 'archive', 'tags')}),
+        (_(u'Publication'), {
+            'classes': ('extrapretty'),
+            'fields': ('published', 'date_available',)}),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.user = get_user_model().objects.get(pk=request.user.pk)
+            obj.date_insert = timezone.now()
+        obj.date_update = timezone.now()
+        obj.save()
+
+admin.site.register(File, FileAdmin)
