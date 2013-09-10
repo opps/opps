dca402f109bb84ddff0d7cf77c106d88a4d7eb7b
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-10 01:47:38 -0300

    remove source application

--- opps/sources/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.utils.translation import ugettext_lazy as _
-
-
-trans_app_label = _('Sources')
--- opps/sources/tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from opps.sources.tests.test_models import *
