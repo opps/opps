739e424cb8a7e84a8ae5f4d7556d8fea51afd86e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-18 16:15:22 -0300

    Write test to container tags get_container_by_channel

--- opps/containers/tests/test_container_tags.py
+++ opps/containers/tests/test_container_tags.py
@@ -8,6 +8,7 @@ from opps.containers.models import Container
 from opps.channels.models import Channel
 
 from ..templatetags.container_tags import get_containers_by
+from ..templatetags.container_tags import get_container_by_channel
 
 
 class GetContainerByTest(TestCase):
@@ -16,8 +17,8 @@ class GetContainerByTest(TestCase):
         user = User.objects.create(username=u'test', password='test')
         site = Site.objects.filter(name=u'example.com').get()
         channel = Channel.objects.create(name=u'Home', slug=u'home',
-                                              description=u'home page',
-                                              site=site, user=user)
+                                         description=u'home page',
+                                         site=site, user=user)
 
         self.container = Container.objects.create(title=u'test',
                                                   user=user,
@@ -37,3 +38,41 @@ class GetContainerByTest(TestCase):
         self.assertTrue(container)
         self.assertEqual(container[0].id, 1)
         self.assertEqual(container[0].user.username, u'test')
+
+
+class GetContainerByChannelTest(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        user = User.objects.create(username=u'test', password='test')
+        site = Site.objects.filter(name=u'example.com').get()
+        channel = Channel.objects.create(name=u'Home', slug=u'home',
+                                              description=u'home page',
+                                              site=site, user=user)
+        channel2 = Channel.objects.create(name=u'Home2', slug=u'home2',
+                                          description=u'home page2',
+                                          site=site, user=user)
+
+        self.count_container_create = [1, 2, 3, 4, 5]
+
+        for i in self.count_container_create:
+            Container.objects.create(title=u'test {}'.format(i),
+                                     user=user,
+                                     published=True,
+                                     site=site,
+                                     channel=channel)
+        Container.objects.create(title=u'test channel 2',
+                                 user=user,
+                                 published=True,
+                                 site=site,
+                                 channel=channel2)
+
+    def test_tag_one_channel(self):
+        get_container = get_container_by_channel('home')
+        self.assertEqual(len(get_container), len(self.count_container_create))
+        for c, i in zip(self.count_container_create[::-1], get_container):
+            self.assertEqual(i.slug, "test-{}".format(c))
+
+    def test_tag_another_channel(self):
+        get_container = get_container_by_channel('home2')
+        self.assertEqual(len(get_container), 1)
+        self.assertEqual(get_container[0].slug, "test-channel-2")
