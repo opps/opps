8e6662a4aaf654ddf18c1c4e733c58db5b9b5579
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-19 17:04:25 -0300

    Add cache in opps menu list via context processors

--- opps/channels/context_processors.py
+++ opps/channels/context_processors.py
@@ -2,6 +2,7 @@
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.sites.models import get_current_site
+from django.core.cache import cache
 
 from .models import Channel
 
@@ -10,10 +11,14 @@ def channel_context(request):
     """ Channel context processors
     """
     site = get_current_site(request)
-    opps_menu = Channel.objects.filter(site=site,
-                                       date_available__lte=timezone.now(),
-                                       published=True,
-                                       show_in_menu=True).order_by('order')
+    opps_menu = cache.get('opps_menu')
+    if not opps_menu:
+        opps_menu = [channel for channel in Channel.objects.filter(
+            site=site,
+            date_available__lte=timezone.now(),
+            published=True,
+            show_in_menu=True).distinct().order_by('order')]
+        cache.set('opps_menu', opps_menu, settings.OPPS_CACHE_EXPIRE)
 
     return {'opps_menu': opps_menu,
             'opps_channel_conf_all': settings.OPPS_CHANNEL_CONF,
