70446e6b13bfc164500b984926475407c8fc9623
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-25 20:11:49 -0300

    add dynamic fields in article forms

--- opps/articles/forms.py
+++ opps/articles/forms.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import json
 from django import forms
 
 from .models import Post, Album, Link
 
 from opps.core.widgets import OppsEditor
 
+from opps.db.models.fields.jsonf import JSONFormField
+from opps.fields.widgets import JSONField
+from opps.fields.models import Field
+
 
 class PostAdminForm(forms.ModelForm):
+    json = JSONFormField(widget=JSONField(), required=False)
+    #json_test = forms.CharField(label='json_test')
 
     multiupload_link = '/fileupload/image/'
+
+    def __init__(self, *args, **kwargs):
+        super(PostAdminForm, self).__init__(*args, **kwargs)
+
+        for field in Field.objects.all():
+            self.fields['json_{}'.format(field.slug)] = forms.CharField(required=False)
 
     class Meta:
         model = Post
