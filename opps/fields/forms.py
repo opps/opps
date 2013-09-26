0270abd8c6509b49a0ce79d66679be2774c270f6
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-26 17:20:26 -0300

    set readonly if exist field

--- opps/fields/forms.py
+++ opps/fields/forms.py
@@ -10,5 +10,11 @@ from opps.core.forms import model_choices
 class FieldAdminForm(forms.ModelForm):
     application = forms.ChoiceField(choices=lazy(model_choices, tuple)())
 
+    def __init__(self, *args, **kwargs):
+        super(FieldAdminForm, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.pk:
+            self.fields['slug'].widget.attrs['readonly'] = True
+
     class Meta:
         model = Field
