ea1a199a35de99671615b0e233577cf6ca66572a
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-26 17:19:52 -0300

    add field option inlne on admin json field

--- opps/fields/admin.py
+++ opps/fields/admin.py
@@ -3,11 +3,25 @@
 from django.contrib import admin
 
 from .forms import FieldAdminForm
-from .models import Field
+from .models import Field, Option, FieldOption
+
+
+class FieldOptionRelatedInline(admin.TabularInline):
+    model = FieldOption
+    fk_name = 'field'
+    raw_id_fields = ['option']
+    actions = None
+    ordering = ('order',)
+    extra = 1
+    classes = ('collapse',)
 
 
 class FieldAdmin(admin.ModelAdmin):
     form = FieldAdminForm
+    prepopulated_fields = {"slug":
+                           ["application", "type", "name"]}
+    inlines = [FieldOptionRelatedInline]
 
 
 admin.site.register(Field, FieldAdmin)
+admin.site.register(Option)
