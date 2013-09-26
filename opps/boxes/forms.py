407fb14a1bf1d9e5531300113aa16bd5a255bbbd
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-09-26 12:26:13 -0300

    User core form to get model_choices

--- opps/boxes/forms.py
+++ opps/boxes/forms.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django import forms
-from django.db import models
-
 from django.utils.functional import lazy
+
 from .models import QuerySet
-
-
-def model_choices():
-    try:
-        return tuple([
-            (u"{0}.{1}".format(app._meta.app_label, app._meta.object_name),
-             u"{0} - {1}".format(app._meta.app_label, app._meta.object_name))
-            for app in models.get_models() if 'opps.' in app.__module__])
-    except ImportError:
-        return tuple([])
+from opps.core.forms import model_choices
 
 
 class QuerySetAdminForm(forms.ModelForm):
