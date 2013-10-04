5d15c16445436e49aa93801a504f9549bc7ea25a
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-04 16:08:17 -0300

    create url entry long puling to notification

--- opps/contrib/notifications/urls.py
+++ opps/contrib/notifications/urls.py
@@ -5,12 +5,15 @@ from django.conf import settings
 
 from opps.core.cache import cache_page
 
-from .views import AsyncServer
+from .views import AsyncServer, LongPullingServer
 
 
 urlpatterns = patterns(
     '',
     url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)\.server$',
         cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
-            AsyncServer.as_view()), name='open'),
+            AsyncServer.as_view()), name='asyncserver'),
+    url(r'^(?P<channel__long_slug>[\w//-]+)/(?P<slug>[\w-]+)\.pulingserver$',
+        cache_page(settings.OPPS_CACHE_EXPIRE_DETAIL)(
+            LongPullingServer.as_view()), name='pulingserver'),
 )
