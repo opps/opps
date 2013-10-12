08bd98b6348c70c2dba890c1809abb67ed769819
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-10-12 16:08:04 -0300

    Regenerator migration, add field mirror

--- opps/containers/migrations/0009_auto__add_mirror.py
+++ opps/containers/migrations/0009_auto__add_mirror.py
@@ -8,6 +8,13 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'Mirror'
+        db.create_table(u'containers_mirror', (
+            (u'container_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['containers.Container'], unique=True, primary_key=True)),
+            ('container', self.gf('django.db.models.fields.related.ForeignKey')(related_name='containers_mirror', to=orm['containers.Container'])),
+        ))
+        db.send_create_signal(u'containers', ['Mirror'])
+
         # Adding M2M table for field mirror_channel on 'ContainerBox'
         m2m_table_name = db.shorten_name(u'containers_containerbox_mirror_channel')
         db.create_table(m2m_table_name, (
@@ -28,6 +35,9 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Deleting model 'Mirror'
+        db.delete_table(u'containers_mirror')
+
         # Removing M2M table for field mirror_channel on 'ContainerBox'
         db.delete_table(db.shorten_name(u'containers_containerbox_mirror_channel'))
 
@@ -148,7 +158,7 @@ class Migration(SchemaMigration):
             'json': ('opps.db.models.fields.jsonf.JSONField', [], {'null': 'True', 'blank': 'True'}),
             'main_image': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "u'containers_container_mainimage'", 'null': 'True', 'on_delete': 'models.SET_NULL', 'to': u"orm['images.Image']"}),
             'main_image_caption': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'mirror_channel': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "u'containers_container_mirror_channel'", 'symmetrical': 'False', 'to': u"orm['channels.Channel']"}),
+            'mirror_channel': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "u'containers_container_mirror_channel'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['channels.Channel']"}),
             'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_containers.container_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
             'published': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
             'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
@@ -172,7 +182,7 @@ class Migration(SchemaMigration):
             'date_insert': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'date_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mirror_channel': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "u'containers_containerbox_mirror_channel'", 'symmetrical': 'False', 'to': u"orm['channels.Channel']"}),
+            'mirror_channel': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "u'containers_containerbox_mirror_channel'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['channels.Channel']"}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '140'}),
             'published': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'db_index': 'True'}),
             'queryset': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'containerbox_querysets'", 'null': 'True', 'to': u"orm['boxes.QuerySet']"}),
@@ -206,6 +216,11 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'image': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['images.Image']", 'null': 'True', 'on_delete': 'models.SET_NULL', 'blank': 'True'}),
             'order': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        u'containers.mirror': {
+            'Meta': {'ordering': "['-date_available']", 'object_name': 'Mirror', '_ormbases': [u'containers.Container']},
+            'container': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'containers_mirror'", 'to': u"orm['containers.Container']"}),
+            u'container_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['containers.Container']", 'unique': 'True', 'primary_key': 'True'})
         },
         u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
