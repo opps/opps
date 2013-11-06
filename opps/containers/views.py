5705a450b8863b9367537e1dceea779b7bf0be7b
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-06 00:53:05 -0200

    remove API view of container application

--- opps/containers/views.py
+++ opps/containers/views.py
@@ -13,23 +13,13 @@ from opps.containers.models import Container
 from opps.channels.models import Channel
 from opps.views.generic.list import ListView
 from opps.views.generic.detail import DetailView
-from opps.api.views.generic.list import ListView as ListAPIView
-from opps.api.views.generic.detail import DetailView as DetailAPIView
 
 
 class ContainerList(ListView):
     model = Container
 
 
-class ContainerAPIList(ListAPIView):
-    model = Container
-
-
 class ContainerDetail(DetailView):
-    model = Container
-
-
-class ContainerAPIDetail(DetailAPIView):
     model = Container
 
 
