5255fd9c9a267d75fea4f83adf22bb8cf0a4de97
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-06 17:55:30 -0200

    Write ContainerBox api

--- opps/api/urls.py
+++ opps/api/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls import patterns, url, include
 
 from tastypie.api import Api
 
-from opps.containers.api import Container
+from opps.containers.api import Container, ContainerBox
+from opps.containers.api import ContainerBoxItens
 from opps.articles.api import Post, Album, Link
 
 from .conf import settings
@@ -12,6 +13,8 @@ from .conf import settings
 
 _api = Api(api_name=settings.OPPS_API_NAME)
 _api.register(Container())
+_api.register(ContainerBox())
+_api.register(ContainerBoxItens())
 _api.register(Post())
 _api.register(Album())
 _api.register(Link())
--- opps/containers/api.py
+++ opps/containers/api.py
@@ -4,10 +4,13 @@ from django.utils import timezone
 from django.conf import settings
 
 from tastypie.resources import ModelResource
+from tastypie.constants import ALL
 
 from opps.api import MetaBase
 
 from .models import Container as ContainerModel
+from .models import ContainerBox as ContainerBoxModel
+from .models import ContainerBoxContainers
 
 
 class Container(ModelResource):
@@ -25,3 +28,20 @@ class Container(ModelResource):
             self.api_name,
             bundle.data['child_class'].lower(),
             bundle.data['id'])
+
+
+class ContainerBoxItens(ModelResource):
+    class Meta:
+        queryset = ContainerBoxContainers.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            "containerbox": ALL
+        }
+
+
+class ContainerBox(ModelResource):
+    class Meta(MetaBase):
+        queryset = ContainerBoxModel.objects.filter(
+            published=True,
+            date_available__lte=timezone.now()
+        )
