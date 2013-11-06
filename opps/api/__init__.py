7d743d02f0069c7db91dd78a525babd5c0f6f4d9
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-11-06 13:11:19 -0200

    Write api MetaBase to tastypie

--- opps/api/__init__.py
+++ opps/api/__init__.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tastypie.constants import ALL
 
+
+class MetaBase:
+    allowed_methods = ['get']
+    filtering = {
+        'site_domain': ALL,
+        'channel_long_slug': ALL,
+        'child_class': ALL,
+        'tags': ALL,
+    }
