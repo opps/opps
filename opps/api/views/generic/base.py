ab73b62420cc82fb11d6a625504f582d0851f06b
Fernando Macedo <fgmacedo@gmail.com>
2013-11-15 16:32:26 -0200

    Removing restframework API Views. Closes #280

--- opps/api/views/generic/base.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from opps.api.conf import settings
-from opps.views.generic.base import View
-
-
-class BaseView(View):
-    def get_paginate_by(self):
-        return settings.OPPS_API_PAGINATE_BY
