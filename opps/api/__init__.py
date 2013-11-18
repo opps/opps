9dc0d24661ef2323e6a825d84585826df5abe7cb
Thiago Avelino <thiago@avelino.xxx>
2013-11-18 16:32:47 -0200

    Create Opps Api Authentication

--- opps/api/__init__.py
+++ opps/api/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from tastypie.constants import ALL
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.models import ApiKey
 
 
 class MetaBase:
@@ -11,3 +13,18 @@ class MetaBase:
         'child_class': ALL,
         'tags': ALL,
     }
+
+
+class ApiAuthentication(ApiKeyAuthentication):
+    def is_authenticated(self, request, **kwargs):
+        try:
+            ApiKey.objects.get(
+                user__username=request.GET.get('username'),
+                key=request.GET.get('api_key'))
+            return True
+        except:
+            ApiKey.DoesNotExist()
+            return False
+
+    def get_identifier(self, request):
+        return request.user.username
