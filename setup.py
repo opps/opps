3e718ba304740553716671d2e5afd712495c33c7
Thiago Avelino <thiago@avelino.xxx>
2013-11-19 10:11:27 -0200

    Fix pep8 on setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import opps
 REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 dependency_links = ['http://github.com/avelino/django-googl/tarball/master'
-                    '#egg=django-googl',]
+                    '#egg=django-googl']
 
 classifiers = [
     'Development Status :: 5 - Production/Stable',
