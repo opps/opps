e339bef374eb2de40c077456d9a5d1c112caa7d0
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-04 13:38:38 -0200

    writter generic methods opps api

--- opps/api/__init__.py
+++ opps/api/__init__.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from tastypie.constants import ALL
-from tastypie.authentication import ApiKeyAuthentication
-from tastypie.models import ApiKey
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+from piston.handler import BaseHandler as Handler
+
+from opps.api.models import ApiKey
 
 
-class MetaBase:
-    allowed_methods = ['get']
-    filtering = {
-        'site_domain': ALL,
-        'channel_long_slug': ALL,
-        'child_class': ALL,
-        'tags': ALL,
-    }
+class BaseHandler(Handler):
+
+    def read(self, request):
+        base = self.model.objects
+        if request.GET.items():
+            return base.filter(**request.GET.dict())
+        return base.all()
 
 
-class ApiAuthentication(ApiKeyAuthentication):
-    def __init__(self, method=[]):
-        self.method = map(str.upper, method)
+class ApiKeyAuthentication(object):
+    def __init__(self, auth_func=authenticate, method=['GET']):
+        self.auth_func = auth_func
+        self.method = method
 
-    def is_authenticated(self, request, **kwargs):
-        try:
-            if request.method in self.method or len(self.method) == 0:
-                ApiKey.objects.get(
-                    user__username=request.GET.get('username'),
-                    key=request.GET.get('api_key'))
+    def is_authenticated(self, request):
+        if request.method == 'GET' and 'GET' in self.method:
             return True
-        except:
-            ApiKey.DoesNotExist()
+
+        method = getattr(request, request.method)
+        try:
+            key = ApiKey.objects.get(
+                user__username=method.get('api_username'),
+                key=method.get('api_key'))
+        except ApiKey.DoesNotExist:
             return False
 
-    def get_identifier(self, request):
-        return request.user.username
+        import pdb; pdb.set_trace()
+        user = self.auth_func(username=key.user.username,
+                              password=key.user.password)
+
+        if not user:
+            return False
+
+        return request.user.is_authenticated()
+
+    def challenge(self):
+        resp = HttpResponse("Authorization Required")
+        resp.status_code = 401
+        return resp
