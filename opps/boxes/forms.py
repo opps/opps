56fde3dccd599a6b8e28a23627e0aca607b5bab9
Ellison Leao <ellisonleao@gmail.com>
2014-01-08 18:10:34 -0200

    Add label on boxes form model field / fix translation file

--- opps/boxes/forms.py
+++ opps/boxes/forms.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.utils.functional import lazy
+from django.utils.translation import ugettext_lazy as _
 
 from .models import QuerySet
 from opps.core.forms import model_choices
 
 
 class QuerySetAdminForm(forms.ModelForm):
-    model = forms.ChoiceField(choices=lazy(model_choices, tuple)())
+    model = forms.ChoiceField(label=_(u'Model'),
+                              choices=lazy(model_choices, tuple)())
 
     class Meta:
         model = QuerySet
