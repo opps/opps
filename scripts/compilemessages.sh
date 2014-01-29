d8a6c0ef5a2164da5563cb464e3aa52e429dde2d
Ellison Leao <ellisonleao@gmail.com>
2014-01-29 19:52:24 -0200

    Fix compilemessages script

--- scripts/compilemessages.sh
+++ scripts/compilemessages.sh
@@ -1,9 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 DIR=$(pwd)
 for i in $(ls opps/)
 do
-	echo "make $i";
-	cd $DIR/opps/$i;
-	django-admin.py compilemessages;
+	if [[ -d $DIR/opps/$i ]]; then
+		cd $DIR/opps/$i;
+		# check if locale folder exists
+		if [[ -e $DIR/opps/$i/locale/ ]]; then
+			echo "make $i";
+			django-admin.py compilemessages -v 3;
+		fi
+	fi
 done
 
