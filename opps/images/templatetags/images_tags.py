fe3daf8284375871cb5092ed08a4006d8200f6a1
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-04-02 02:44:25 -0300

    Writing templatetags all_images_check_permission check access permission on before get the images

--- opps/images/templatetags/images_tags.py
+++ opps/images/templatetags/images_tags.py
@@ -1,11 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django import template
+from django.template import Node, TemplateSyntaxError, Variable
 from django.conf import settings
+from django.utils.translation import ugettext as _
 from ..generate import image_url as url
 
 
 register = template.Library()
+
+
+class AllImagesCheckPermissionForObjectsNode(Node):
+    def __init__(self, obj, name):
+        self.obj = Variable(obj)
+        self.name = name
+
+    def render(self, context):
+        check_published = True
+
+        try:
+            user = context['request'].user
+            if user.is_staff or user.is_superuser:
+                check_published = False
+        except:
+            pass
+
+        print check_published
+        obj = self.obj.resolve(context)
+        context[self.name] = obj.all_images(check_published)
+        return ''
+
+
+@register.tag(name='all_images_check_permission')
+def all_images_check_permission(parser, token):
+    """
+    {% all_images_check_permission object as images %}
+    """
+    try:
+        parans = token.split_contents()
+    except ValueError:
+        raise TemplateSyntaxError(
+            _('tag requires exactly two arguments'))
+    if len(parans) != 4:
+        raise TemplateSyntaxError(
+            _('tag requires exactly three arguments'))
+    if parans[2] != 'as':
+        raise TemplateSyntaxError(
+            _("second argument to tag must be 'as'"))
+    return AllImagesCheckPermissionForObjectsNode(parans[1], parans[3])
 
 
 @register.simple_tag
