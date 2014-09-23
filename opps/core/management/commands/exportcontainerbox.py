1e5a8ba8c9271a28dfd85274def00474c0d4a711
Paulo R <proberto.macedo@gmail.com>
2014-09-23 01:13:21 -0300

    Makes compatible with python 2.6

--- opps/core/management/commands/exportcontainerbox.py
+++ opps/core/management/commands/exportcontainerbox.py
@@ -1,8 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.core import serializers
+
+from opps.boxes.models import QuerySet
 from opps.channels.models import Channel
 from opps.containers.models import ContainerBox
-from opps.boxes.models import QuerySet
 
 
 class Command(BaseCommand):
@@ -11,6 +12,6 @@ class Command(BaseCommand):
         models = [Channel, ContainerBox, QuerySet]
         for m in models:
             data = serializers.serialize("json", m.objects.all())
-            out = open("opps_{}.json".format(m.__name__), "w")
+            out = open("opps_{0}.json".format(m.__name__), "w")
             out.write(data)
             out.close()
