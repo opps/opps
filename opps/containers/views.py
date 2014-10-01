cbf24c01d977a3328c54fd4cbd199764b820b7df
Paulo R <proberto.macedo@gmail.com>
2014-09-30 22:30:45 -0300

    Fixes haystack search

--- opps/containers/views.py
+++ opps/containers/views.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.core.paginator import Paginator, InvalidPage
-from django.http import Http404
 
 from haystack.views import SearchView
 
@@ -21,12 +18,3 @@ class ContainerDetail(DetailView):
 class Search(SearchView):
     def get_results(self):
         return self.form.search().order_by('-date_available')
-
-    def build_page(self):
-        paginator = Paginator(self.results, self.results_per_page)
-        try:
-            paginator.page(int(self.request.GET.get('page', 1)))
-        except InvalidPage:
-            raise Http404("No such page!")
-
-        return (None, self.results)
