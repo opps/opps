54d49a10f26b53926a8485abfcbd879e65a2a1d7
Guilherme Rezende <guilhermebr@gmail.com>
2014-10-17 10:44:09 -0300

    Fix update link redirection bug

--- opps/articles/signals.py
+++ opps/articles/signals.py
@@ -6,6 +6,6 @@ from django.contrib.redirects.models import Redirect
 def redirect_generate(sender, instance, created, **kwargs):
     obj, create = Redirect.objects.get_or_create(
         old_path=instance.get_absolute_url(),
-        site=instance.site,
-        new_path=instance.url)
+        site=instance.site)
+    obj.new_path = instance.url
     obj.save()
