4368812907d9828ee9323af90e1af79cb44c1641
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-12-30 12:24:30 -0200

    refactoring AdminViewPermission get level channel today permission rules via channel, user just see the news channel it is part

--- opps/core/permissions/admin.py
+++ opps/core/permissions/admin.py
@@ -5,66 +5,85 @@ from django.conf import settings
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.admin.widgets import FilteredSelectMultiple
+
+from opps.channels.models import Channel
+
 from .models import Permission
 
 
 class AdminViewPermission(admin.ModelAdmin):
 
-    site_lookup = 'site_iid__in'
+    @property
+    def site_lookup(self):
+        return 'site_iid__in'
+
+    def permission_in(self, user):
+        sites_id = []
+        channels_id = []
+        p = Permission.objects.filter(
+            user=user,
+            date_available__lte=timezone.now(),
+            published=True
+        ).values_list('site__id', 'channel__id')
+        for s,c in p:
+            sites_id.append(s)
+            channels_id.append(c)
+
+        return {'sites_id': set(sites_id), 'channels_id': channels_id}
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if settings.OPPS_MULTISITE_ADMIN and not request.user.is_superuser:
-            sites_id = Permission.objects.filter(
-                user=request.user,
-                date_available__lte=timezone.now(),
-                published=True
-            ).values_list('site__id', flat=True)
+            obj = self.permission_in(request.user)
 
             if db_field.name in ['site']:
-                kwargs['queryset'] = Site.objects.filter(id__in=sites_id)
+                kwargs['queryset'] = Site.objects.filter(
+                    id__in=obj.get('sites_id', []))
 
-        result = super(AdminViewPermission, self).formfield_for_foreignkey(
-            db_field, request, **kwargs
-        )
+            if db_field.name in ['channel']:
+                kwargs['queryset'] = Channel.objects.filter(
+                    id__in=obj.get('channels_id', []))
 
-        return result
+        return super(AdminViewPermission, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
 
     def queryset(self, request):
         qs = super(AdminViewPermission, self).queryset(request)
         if not settings.OPPS_MULTISITE_ADMIN or request.user.is_superuser:
             return qs
 
-        sites_id = Permission.objects.filter(
-            user=request.user,
-            date_available__lte=timezone.now(),
-            published=True
-        ).values_list('site__id', flat=True)
+        obj = self.permission_in(request.user)
 
-        return qs.filter(**{self.site_lookup: sites_id})
+        if self.__class__.__name__ == 'ChannelAdmin':
+            return qs.filter(**{
+                'site__id__in': obj.get('sites_id', []),
+                'id__in': obj.get('channels_id', [])})
+        return qs.filter(**{
+            'site_iid__in': obj.get('sites_id', []),
+            'channel__id__in': obj.get('channels_id', [])})
 
     def get_form(self, request, obj=None, **kwargs):
-
-        form = super(
-            AdminViewPermission, self
-        ).get_form(request, obj, **kwargs)
-
-        sites_id = Permission.objects.filter(
-            user=request.user,
-            date_available__lte=timezone.now(),
-            published=True
-        ).values_list('site__id', flat=True)
+        form = super(AdminViewPermission, self).get_form(
+            request, obj, **kwargs)
+        obj = self.permission_in(request.user)
 
         try:
-            attrs = {}
+            attrs_mirror = {}
             if settings.OPPS_MULTISITE_ADMIN and not request.user.is_superuser:
-                qs = form.base_fields['mirror_site'].queryset
-                form.base_fields['mirror_site'].queryset = qs.filter(
-                    id__in=sites_id
-                )
-                attrs = {'disabled': 'disabled'}
+                qs_mirror = form.base_fields['mirror_site'].queryset
+                form.base_fields['mirror_site'].queryset = qs_mirror.filter(
+                    id__in=obj.get('sites_id', []))
+                attrs_mirror = {'disabled': 'disabled'}
+
             form.base_fields['mirror_site'].widget = FilteredSelectMultiple(
-                _("Mirror site"), is_stacked=False, attrs=attrs
-            )
+                _("Mirror site"), is_stacked=False, attrs=attrs_mirror)
+        except:
+            pass
+
+        try:
+            if settings.OPPS_MULTISITE_ADMIN and not request.user.is_superuser:
+                qs_channel = form.base_fields['channel'].queryset
+                form.base_fields['channel'].queryset = qs_channel.filter(
+                    id__in=obj.get('channels_id', []))
         except:
             pass
 
