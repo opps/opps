7aab3c030a80642a97a032565f2a0683b1f12209
Thiago Avelino <thiagoavelinoster@gmail.com>
2014-12-29 23:52:54 -0200

    migrate multisite.models to permissions.models

---
+++ opps/core/permissions/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from opps.core.models import Publishable
+from django.utils.translation import ugettext_lazy as _
+
+
+class Permission(Publishable):
+    class Meta:
+        verbose_name = _(u'Permission')
+        verbose_name_plural = _(u'Permissions')
+
+    def __unicode__(self):
+        return u'{0} em {1}'.format(self.user, self.site)
