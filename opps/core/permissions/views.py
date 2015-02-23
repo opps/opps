060303fa512f2c9792e4e0af161aade377590cd2
Ervilis Souza <ervilisviana@gmail.com>
2015-02-23 18:01:28 -0300

    Adding filter by Site and fix filter by Publisher on autocomplete

--- opps/core/permissions/views.py
+++ opps/core/permissions/views.py
@@ -3,6 +3,7 @@ from grappelli.views.related import AutocompleteLookup
 
 from django.db.models import Q
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 from opps.core.models import Publisher, Channeling
 from opps.channels.models import Channel
@@ -25,11 +26,13 @@ class OppsAutocompleteLookup(AutocompleteLookup):
         if self.model == Channel:
             filters |= (Q(id__in=permissions['channels_id']) |
                         Q(site_id__in=permissions['sites_id']))
+        elif self.model == Site:
+            filters |= Q(id__in=permissions['all_sites_id'])
         else:
             if issubclass(self.model, Channeling):
                 filters |= Q(channel_id__in=permissions['channels_id'])
 
             if issubclass(self.model, Publisher):
-                filters |= Q(site_iid__in=permissions['all_sites_id'])
+                filters |= Q(site_iid__in=permissions['sites_id'])
 
         return qs.filter(filters)
